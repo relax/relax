11b95af9bf140f57455b03fbde442e8f34f72de1
Bruno Mota <bruno12mota@gmail.com>
2015-10-01 15:46:56 +0100

    collection combobox created

---
+++ lib/components/collection-combobox.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Component} from 'relax-framework';
+import Combobox from './combobox';
+import forEach from 'lodash.foreach';
+
+export default class CollectionCombobox extends Component {
+  getInitialState () {
+    return {
+      entries: []
+    };
+  }
+
+  getInitialCollections () {
+    return {
+      entries: this.props.store.getCollection()
+    };
+  }
+
+  render () {
+    let labels = [], values = [];
+
+    forEach(this.state.entries, (entry) => {
+      labels.push(entry.title);
+      values.push(entry.slug);
+    });
+
+    return (
+      <Combobox
+        labels={labels}
+        values={values}
+        value={this.props.value}
+        onChange={this.props.onChange}
+      />
+    );
+  }
+}
+
+CollectionCombobox.propTypes = {
+  store: React.PropTypes.any.isRequired,
+  value: React.PropTypes.string.isRequired,
+  onChange: React.PropTypes.func.isRequired
+};
