1a69c412e3455b560ad7d29fd6fe077f067eb7e2
José Magalhães <magalhas@gmail.com>
2015-10-18 05:38:10 +0100

    refactor Settings into smart component #98

--- lib/components/admin/panels/settings/index.jsx
+++ lib/components/admin/panels/settings/index.jsx
@@ -1,27 +1,26 @@
 import React, {PropTypes} from 'react';
 import {Component} from 'relax-framework';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
 import cx from 'classnames';
 
 import OptionsList from '../../../options-list';
 import FormState from '../../../form-state';
 import options from './options';
 
-import * as settingsActions from '../../../../actions/settings';
-
-@connect(
-  (state) => ({
-    settings: state.settings.data
-  }),
-  (dispatch) => bindActionCreators(settingsActions, dispatch)
-)
 export default class Settings extends Component {
   static fragments = {
     settings: {
       _id: 1,
       value: 1
     }
+  }
+
+  static propTypes = {
+    settings: PropTypes.object.isRequired,
+    changeSettingValue: PropTypes.func.isRequired,
+    saveSettings: PropTypes.func.isRequired,
+    saving: PropTypes.boolean,
+    state: PropTypes.string.isRequired,
+    onSubmit: PropTypes.func.isRequired,
   }
 
   static settings = [
@@ -36,50 +35,6 @@ export default class Settings extends Component {
     'mailTo'
   ]
 
-  static propTypes = {
-    settings: PropTypes.object.isRequired,
-    changeSettingValue: PropTypes.func.isRequired,
-    saveSettings: PropTypes.func.isRequired
-  }
-
-  getInitialState () {
-    return {
-      state: false
-    };
-  }
-
-  onChange (id, value) {
-    this.props.changeSettingValue(id, value);
-  }
-
-  outSuccess () {
-    this.setState({
-      state: false
-    });
-  }
-
-  submit (event) {
-    event.preventDefault();
-
-    clearTimeout(this.successTimeout);
-    this.setState({
-      state: 'loading'
-    });
-
-    this.props.saveSettings(this.constructor.fragments, this.props.settings)
-      .then(() => {
-        this.setState({
-          state: 'success'
-        });
-        this.successTimeout = setTimeout(this.outSuccess.bind(this), 3000);
-      })
-      .catch(() => {
-        this.setState({
-          state: 'error'
-        });
-      });
-  }
-
   render () {
     return (
       <div>
@@ -88,8 +43,13 @@ export default class Settings extends Component {
         </div>
         <div className='admin-scrollable'>
           <div className='list white-options'>
-            <OptionsList options={options} values={this.props.settings} onChange={this.onChange.bind(this)} />
-            <a href='#' className={cx('button', 'button-primary', this.state.saving && 'disabled')} onClick={this.submit.bind(this)}>Submit changes</a>
+            <OptionsList options={options} values={this.props.settings} onChange={this.onChange} />
+            <a
+                href='#'
+                className={cx('button', 'button-primary', this.props.saving && 'disabled')}
+                onClick={this.props.onSubmit}>
+              Submit changes
+            </a>
             {this.renderSaving()}
           </div>
         </div>
@@ -100,18 +60,18 @@ export default class Settings extends Component {
   renderSaving () {
     let message = false;
 
-    if (this.state.state === 'loading') {
+    if (this.props.state === 'loading') {
       message = 'Saving changes';
-    } else if (this.state.state === 'error') {
+    } else if (this.props.state === 'error') {
       message = 'Error saving changes';
-    } else if (this.state.state === 'success') {
+    } else if (this.props.state === 'success') {
       message = 'Changes saved';
     }
 
     if (message !== false) {
       return (
         <FormState
-          state={this.state.state}
+          state={this.props.state}
           message={message}
         />
       );
