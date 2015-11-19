7aaa16f4ea892a42e1817508b95719e8a316c69c
Bruno Mota <bruno12mota@gmail.com>
2015-11-19 15:31:48 +0000

    config identation fix

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var rc = require('rc');
 
 module.exports = rc('relax', {
-   port: 8080,
-   devPort: 8181,
-   db: {
-      uri: 'mongodb://localhost/relax'
-   }
+  port: 8080,
+  devPort: 8181,
+  db: {
+    uri: 'mongodb://localhost/relax'
+  }
 });
