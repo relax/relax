e3750aac94bdb2aa9e41dce15ed18e2d01b271e1
Bruno Mota <bruno12mota@gmail.com>
2015-11-20 16:01:30 +0000

    dynamic list element prevent overing and selecting outside elements when linking #126

--- lib/containers/page-builder.jsx
+++ lib/containers/page-builder.jsx
@@ -21,6 +21,20 @@ function getElementPath (selectedElement, data) {
   return result;
 }
 
+function inPath (elementId, targetId, data) {
+  let found = false;
+  let current = data[elementId];
+
+  while (!found && current.parent) {
+    if (current.parent === targetId) {
+      found = true;
+    }
+    current = data[current.parent];
+  }
+
+  return found;
+}
+
 function getPageBuilder (pageBuilder, draft) {
   const data = draft.data.data;
   const result = {
@@ -32,7 +46,17 @@ function getPageBuilder (pageBuilder, draft) {
     userExpanded: draft.userExpanded
   };
 
-  if (pageBuilder.selectedId) {
+  if (pageBuilder.linkingData) {
+    // Check if inside the linking data holder
+    if (pageBuilder.overedId && !inPath(pageBuilder.overedId, pageBuilder.linkingDataElementId, data)) {
+      result.overedId = null;
+    }
+    if (pageBuilder.selectedId && !inPath(pageBuilder.selectedId, pageBuilder.linkingDataElementId, data)) {
+      result.selectedId = null;
+    }
+  }
+
+  if (pageBuilder.selectedId && result.selectedId) {
     const selectedElement = data[pageBuilder.selectedId];
     result.selectedElement = selectedElement;
     result.selectedParent = selectedElement && selectedElement.parent;
