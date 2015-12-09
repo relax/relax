c3d88f99a12aecdecf3f8a8eb416525d97b0286a
Bruno Mota <bruno12mota@gmail.com>
2015-12-09 11:00:18 +0000

    schemas data types revision

--- lib/components/admin/panels/schema/builder/property-types.jsx
+++ lib/components/admin/panels/schema/builder/property-types.jsx
@@ -10,52 +10,16 @@ export default class PropertyTypes extends Component {
     onChange: React.PropTypes.func.isRequired
   }
 
-  getInitState () {
-    return {
-      expanded: false
-    };
-  }
-
   onClick (type) {
     this.props.onChange(type);
-  }
-
-  toggleMore () {
-    this.setState({
-      expanded: !this.state.expanded
-    });
   }
 
   render () {
     return (
       <div className='property-types'>
-        {schemaTypesOrdered.common.map(this.renderType, this)}
-        {this.renderExpanded()}
+        {schemaTypesOrdered.map(this.renderType, this)}
       </div>
     );
-  }
-
-  renderExpanded () {
-    let result;
-    if (this.state.expanded) {
-      result = (
-        <div className='advanced-types'>
-          <div className='toggle-more' key='toggle-more' onClick={this.toggleMore.bind(this)}>
-            <span>Hide advanced options</span>
-            <i className='material-icons'>expand_more</i>
-          </div>
-          {schemaTypesOrdered.more.map(this.renderType, this)}
-        </div>
-      );
-    } else {
-      result = (
-        <div className='toggle-more' onClick={this.toggleMore.bind(this)}>
-          <span>Show advanced options</span>
-          <i className='material-icons'>expand_less</i>
-        </div>
-      );
-    }
-    return result;
   }
 
   renderType (type, index) {
