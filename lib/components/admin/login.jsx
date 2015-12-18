fbef93bf1010e29e36eef3ef88f8ee38c328b7c4
Bruno Mota <bruno12mota@gmail.com>
2015-12-18 12:56:51 +0000

    login validation and error output

--- lib/components/admin/login.jsx
+++ lib/components/admin/login.jsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import {Component} from 'relax-framework';
 
 export default class Login extends Component {
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    fieldChange: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+    error: PropTypes.string
+  }
+
+  onChange (id, event) {
+    this.props.fieldChange(id, event.target.value);
+  }
+
   render () {
     return (
       <div className='page-init white-options'>
         <h1>Login</h1>
-        <form action='/admin/login' method='post' ref='form'>
+        <form onSubmit={this.props.onSubmit}>
           <div className='option'>
             <div className='label'>Username</div>
             <div className='input'>
-              <input type='text' name='username' placeholder='Enter Username' />
+              <input type='text' name='username' placeholder='Enter Username' value={this.props.username} onChange={this.onChange.bind(this, 'username')} />
             </div>
           </div>
           <div className='option'>
             <div className='label'>Password</div>
             <div className='input'>
-              <input type='password' name='password' placeholder='Password' />
+              <input type='password' name='password' placeholder='Password' value={this.props.password} onChange={this.onChange.bind(this, 'password')} />
             </div>
           </div>
           <a className='button button-primary full' href='#' onClick={this.props.onSubmit}>Login</a>
-          <input type='submit' hidden='true' />
+          {this.props.error && <div className='error'>{this.props.error}</div>}
+          <input type='submit' hidden />
         </form>
       </div>
     );
--- lib/containers/admin/login.jsx
+++ lib/containers/admin/login.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import request from 'superagent';
+import React, {PropTypes} from 'react';
 import {Component} from 'relax-framework';
 
 import Login from '../../components/admin/login';
 
 export default class LoginContainer extends Component {
+  static propTypes = {
+    history: PropTypes.object.isRequired
+  }
+
+  getInitState () {
+    return {
+      username: '',
+      password: ''
+    };
+  }
+
   onSubmit (event) {
     event.preventDefault();
-    this.refs.login.refs.form.submit();
+    const {username, password} = this.state;
+
+    request
+      .post('/admin/login')
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .send({username, password})
+      .end((error, res) => {
+        if (error) {
+          this.setState({
+            error: res.body.message
+          });
+        } else {
+          window.location.href = '/admin';
+        }
+      });
+  }
+
+  fieldChange (id, value) {
+    this.setState({
+      [id]: value
+    });
   }
 
   render () {
@@ -16,6 +49,7 @@ export default class LoginContainer extends Component {
         {...this.props}
         {...this.state}
         onSubmit={::this.onSubmit}
+        fieldChange={::this.fieldChange}
       />
     );
   }
