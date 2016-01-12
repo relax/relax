80e5e6fd8709bd66e9aad1612cefbc97b057f50a
Bruno Mota <bruno12mota@gmail.com>
2016-01-12 14:28:55 +0000

    new login and init design

--- lib/components/admin/login.jsx
+++ lib/components/admin/login.jsx
@@ -17,22 +17,23 @@ export default class Login extends Component {
   render () {
     return (
       <div className='page-init white-options'>
-        <h1>Login</h1>
+        <div className='logo'>
+          <img src='/images/admin/logo_big.png' width='150' />
+          <div className='version'>beta</div>
+        </div>
+        <h1>Welcome back!</h1>
+        <h3>Login with your account below to get started</h3>
         <form onSubmit={this.props.onSubmit}>
-          <div className='option'>
-            <div className='label'>Username</div>
-            <div className='input'>
-              <input type='text' name='username' placeholder='Enter Username' value={this.props.username} onChange={this.onChange.bind(this, 'username')} />
-            </div>
-          </div>
-          <div className='option'>
-            <div className='label'>Password</div>
-            <div className='input'>
-              <input type='password' name='password' placeholder='Password' value={this.props.password} onChange={this.onChange.bind(this, 'password')} />
-            </div>
-          </div>
-          <a className='button button-primary full' href='#' onClick={this.props.onSubmit}>Login</a>
-          {this.props.error && <div className='error'>{this.props.error}</div>}
+          <label>
+            <i className='material-icons'>person_outline</i>
+            <input type='text' name='username' placeholder='Username' value={this.props.username} onChange={this.onChange.bind(this, 'username')} />
+          </label>
+          <label>
+            <i className='material-icons'>lock_outline</i>
+            <input type='password' name='password' placeholder='Password' value={this.props.password} onChange={this.onChange.bind(this, 'password')} />
+          </label>
+          <a className='button button-primary full' href='#' onClick={this.props.onSubmit}>Let's get started</a>
+          {<div className='error'>{this.props.error && this.props.error || ' '}</div>}
           <input type='submit' hidden />
         </form>
       </div>
