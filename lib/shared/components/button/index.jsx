3dff89102fba055f4088d9f3800ce353a125ca63
Bruno Mota <bruno12mota@gmail.com>
2016-02-07 17:35:58 +0100

    button component

---
+++ lib/shared/components/button/index.jsx
@@ -0,0 +1,40 @@
+import cx from 'classnames';
+import forEach from 'lodash.foreach';
+import React, {PropTypes} from 'react';
+import {Component} from 'relax-framework';
+
+import styles from './index.less';
+
+export default class Button extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    onClick: PropTypes.func.isRequired,
+    style: PropTypes.object,
+    primary: PropTypes.boolean,
+    full: PropTypes.boolean,
+    big: PropTypes.boolean
+  };
+
+  static defaultProps = {
+    primary: true,
+    full: false,
+    big: false
+  };
+
+  render () {
+    const {onClick, ...classes} = this.props;
+
+    let className = styles.button;
+    forEach(classes, (value, key) => {
+      if (styles[key] && value) {
+        className = cx(className, styles[key]);
+      }
+    });
+
+    return (
+      <button className={className} onClick={onClick} style={this.props.style}>
+        {this.props.children}
+      </button>
+    );
+  }
+}
