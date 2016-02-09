2c15f2a2f9ca6c2fb6042394c55b237c2351e3e5
Bruno Mota <bruno12mota@gmail.com>
2016-02-09 17:21:51 +0100

    admin side menu ui

---
+++ lib/shared/screens/admin/shared/components/menu/index.jsx
@@ -0,0 +1,82 @@
+import A from 'components/a';
+import React, {PropTypes} from 'react';
+import {Component} from 'relax-framework';
+
+import styles from './index.less';
+import User from './user';
+
+export default class Menu extends Component {
+  static menuData = [
+    {
+      label: 'General Settings',
+      icon: 'nc-icon-outline ui-1_preferences-container',
+      link: '/admin'
+    },
+    'sep',
+    {
+      label: 'Pages',
+      icon: 'nc-icon-outline design_window-paragraph',
+      link: '/admin/pages'
+    },
+    {
+      label: 'Templates',
+      icon: 'nc-icon-outline ui-2_webpage',
+      link: '/admin/templates'
+    },
+    'sep',
+    {
+      label: 'Media',
+      icon: 'nc-icon-outline media-1_album',
+      link: '/admin/media'
+    },
+    {
+      label: 'Menus',
+      icon: 'nc-icon-outline design_bullet-list-67',
+      link: '/admin/menus'
+    },
+    {
+      label: 'Fonts',
+      icon: 'nc-icon-outline design_text',
+      link: '/admin/fonts'
+    },
+    {
+      label: 'Colors',
+      icon: 'nc-icon-outline design_palette',
+      link: '/admin/colors'
+    },
+    {
+      label: 'Users',
+      icon: 'nc-icon-outline users_multiple-19',
+      link: '/admin/users'
+    }
+  ];
+
+  render () {
+    return (
+      <div className={styles.root}>
+        <div className={styles.menu}>
+          {Menu.menuData.map(this.renderEntry, this)}
+          <User user={{name: 'Bruno Mota', email: 'bruno12mota@gmail.com'}} />
+        </div>
+        <div className={styles.list}>
+
+        </div>
+      </div>
+    );
+  }
+
+  renderEntry (entry, key) {
+    let result;
+    if (entry === 'sep') {
+      result = <div className={styles.sepperator}/>;
+    } else {
+      result = (
+        <A href={entry.link} className={styles.button} key={key}>
+          <i className={entry.icon}></i>
+          <span>{entry.label}</span>
+        </A>
+      );
+    }
+    return result;
+  }
+}
---
+++ lib/shared/screens/admin/shared/components/menu/user.jsx
@@ -0,0 +1,50 @@
+import cx from 'classnames';
+import utils from 'helpers/utils';
+import React, {PropTypes} from 'react';
+import {Component} from 'relax-framework';
+
+import styles from './user.less';
+
+export default class User extends Component {
+  static propTypes = {
+    user: PropTypes.object.isRequired
+  };
+
+  getInitState () {
+    return {
+      opened: false
+    };
+  }
+
+  toggleOpened () {
+    this.setState({
+      opened: !this.state.opened
+    });
+  }
+
+  render () {
+    const {email, name} = this.props.user;
+    const {opened} = this.state;
+    const url = utils.getGravatarImage(email, 50);
+
+    return (
+      <div className={styles.root}>
+        <div className={styles.thumbnail}>
+          <div className={styles.blurred}>
+            <img src={url} />
+          </div>
+          <div className={styles.image}>
+            <img src={url} />
+          </div>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.name}>{name}</div>
+          <div className={styles.role}>Administrator</div>
+        </div>
+        <button className={styles.toggle} onClick={::this.toggleOpened}>
+          <i className={cx('nc-icon-mini', opened ? 'arrows-1_minimal-up' : 'arrows-1_minimal-down')}></i>
+        </button>
+      </div>
+    );
+  }
+}
