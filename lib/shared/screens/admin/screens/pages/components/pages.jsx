720d76ae5f582ddf82ef3191a58b6d7dbf3a46c1
Bruno Mota <bruno12mota@gmail.com>
2016-02-09 17:21:51 +0100

    admin side menu ui

---
+++ lib/shared/screens/admin/screens/pages/components/pages.jsx
@@ -0,0 +1,13 @@
+import Menu from 'components/menu';
+import React, {PropTypes} from 'react';
+import {Component} from 'relax-framework';
+
+export default class Pages extends Component {
+  render () {
+    return (
+      <div>
+        <Menu />
+      </div>
+    );
+  }
+}
