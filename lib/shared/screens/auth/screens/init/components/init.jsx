293284d559a2640b90d516fe7e4e93301a336d1d
Bruno Mota <bruno12mota@gmail.com>
2016-02-10 10:41:23 +0100

    admin init icons nucleo replacement

--- lib/shared/screens/auth/screens/init/components/init.jsx
+++ lib/shared/screens/auth/screens/init/components/init.jsx
@@ -24,19 +24,19 @@ export default class Init extends Component {
         <div className={authStyles.subTitle}>Register the first user to start building your website in a breeze.</div>
         <form className={authStyles.form} onSubmit={this.props.onSubmit}>
           <label>
-            <i className='material-icons'>person_outline</i>
+            <i className='nc-icon-outline users_single-03'></i>
             <input type='text' name='username' placeholder='Username' value={username} onChange={this.onChange.bind(this, 'username')} />
           </label>
           <label>
-            <i className='material-icons'>lock_outline</i>
+            <i className='nc-icon-outline ui-1_lock'></i>
             <input type='password' name='password' placeholder='Password' value={password} onChange={this.onChange.bind(this, 'password')} />
           </label>
           <label>
-            <i className='material-icons smaller'>face</i>
+            <i className='nc-icon-outline users_circle-08'></i>
             <input type='text' name='name' placeholder='Name' value={name} onChange={this.onChange.bind(this, 'name')} />
           </label>
           <label>
-            <i className='material-icons smaller'>email</i>
+            <i className='nc-icon-outline ui-1_email-85'></i>
             <input type='text' name='email' placeholder='Email' value={email} onChange={this.onChange.bind(this, 'email')} />
           </label>
           <Button primary full big onClick={this.props.onSubmit} style={{marginTop: 40}}>Let's get started</Button>
