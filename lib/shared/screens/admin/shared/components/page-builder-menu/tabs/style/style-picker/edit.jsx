543195c70abb1b47d67ad73feced6444de8b3b4e
Bruno Mota <bruno12mota@gmail.com>
2016-02-21 17:58:04 +0100

    element styles tab

---
+++ lib/shared/screens/admin/shared/components/page-builder-menu/tabs/style/style-picker/edit.jsx
@@ -0,0 +1,34 @@
+import getElementStyleValues from 'helpers/get-element-style-values';
+import Component from 'components/component';
+import OptionsList from 'components/options-list';
+import React from 'react';
+
+export default class Edit extends Component {
+  static propTypes = {
+    display: React.PropTypes.string.isRequired,
+    styleOptions: React.PropTypes.object.isRequired,
+    selectedStyle: React.PropTypes.object.isRequired,
+    onChange: React.PropTypes.func.isRequired
+  };
+
+  render () {
+    return (
+      <div className='style-picker-options'>
+        {this.renderOptions()}
+      </div>
+    );
+  }
+
+  renderOptions () {
+    const {styleOptions, selectedStyle, display} = this.props;
+    const values = getElementStyleValues(styleOptions.defaults, selectedStyle.options, selectedStyle.displayOptions, display);
+
+    return (
+      <OptionsList
+        options={styleOptions.options}
+        values={values}
+        onChange={this.props.onChange}
+      />
+    );
+  }
+}
