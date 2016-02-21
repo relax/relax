8afe359c37e3d2443670ade89d0e4a6782f6cfe0
Bruno Mota <bruno12mota@gmail.com>
2016-02-21 18:43:57 +0100

    box shadow input option

---
+++ lib/shared/screens/admin/shared/components/input-options/box-shadow/edit.jsx
@@ -0,0 +1,68 @@
+import Component from 'components/component';
+import React, {PropTypes} from 'react';
+
+export default class Edit extends Component {
+  static propTypes = {
+    shadow: PropTypes.object.isRequired,
+    changeShadow: PropTypes.func.isRequired,
+    OptionsList: PropTypes.object.isRequired
+  };
+
+  static options = [
+    {
+      type: 'Columns',
+      options: [
+        {
+          label: 'Color',
+          type: 'Color',
+          id: 'color'
+        },
+        {
+          label: 'Blur',
+          type: 'Pixels',
+          id: 'blur'
+        }
+      ]
+    },
+    {
+      type: 'Columns',
+      options: [
+        {
+          label: 'X',
+          type: 'Pixels',
+          id: 'x'
+        },
+        {
+          label: 'Y',
+          type: 'Pixels',
+          id: 'y'
+        }
+      ]
+    },
+    {
+      type: 'Columns',
+      options: [
+        {
+          label: 'Spread',
+          type: 'Pixels',
+          id: 'spread'
+        },
+        {
+          label: 'Inset/Outset',
+          type: 'ShadowPosition',
+          id: 'type'
+        }
+      ]
+    }
+  ];
+
+  render () {
+    const {shadow, OptionsList} = this.props;
+
+    return (
+      <div className='box-shadow-edit white-options'>
+        <OptionsList options={Edit.options} values={shadow} onChange={this.props.changeShadow} />
+      </div>
+    );
+  }
+}
