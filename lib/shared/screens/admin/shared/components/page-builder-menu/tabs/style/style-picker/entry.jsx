27b041c486955bad7efecaa6368c7b0c8b59c861
Bruno Mota <bruno12mota@gmail.com>
2016-02-21 17:58:04 +0100

    element styles tab

---
+++ lib/shared/screens/admin/shared/components/page-builder-menu/tabs/style/style-picker/entry.jsx
@@ -0,0 +1,90 @@
+import Component from 'components/component';
+import OptionsMenu from 'components/options-menu';
+import React, {PropTypes} from 'react';
+
+export default class Entry extends Component {
+  static propTypes = {
+    entry: PropTypes.object.isRequired,
+    onClick: PropTypes.func.isRequired,
+    styleOptions: PropTypes.object.isRequired,
+    removeStyle: PropTypes.func.isRequired,
+    duplicateStyle: PropTypes.func.isRequired
+  };
+
+  getInitState () {
+    return {
+      options: false
+    };
+  }
+
+  openOptions (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.setState({
+      options: true
+    });
+  }
+
+  onMouseLeave () {
+    if (this.state.options) {
+      this.setState({
+        options: false
+      });
+    }
+  }
+
+  onClick (event) {
+    event.preventDefault();
+    this.props.onClick(this.props.entry._id);
+  }
+
+  duplicate () {
+    this.props.duplicateStyle(this.props.entry);
+  }
+
+  remove () {
+    this.props.removeStyle(this.props.entry._id);
+  }
+
+  render () {
+    return (
+      <div className='entry' onClick={::this.onClick} onMouseLeave={::this.onMouseLeave}>
+        <div className='info-holder'>
+          <span className='title'>{this.props.entry.title}</span>
+          {this.renderOptionsButton()}
+          {this.renderInfo()}
+        </div>
+      </div>
+    );
+  }
+
+  renderOptionsButton () {
+    if (this.props.entry._id !== 'no_style') {
+      return (
+        <span className='options-btn' onClick={::this.openOptions}>
+          <i className='material-icons'>more_horiz</i>
+          {this.renderOptionsMenu()}
+        </span>
+      );
+    }
+  }
+
+  renderOptionsMenu () {
+    if (this.state.options) {
+      return (
+        <OptionsMenu options={[
+          {label: 'Duplicate', action: ::this.duplicate, icon: 'fa fa-copy'},
+          {label: 'Remove', action: ::this.remove, icon: 'fa fa-trash-o'}
+        ]} />
+      );
+    }
+  }
+
+  renderInfo () {
+    if (this.props.styleOptions.getIdentifierLabel && this.props.entry._id !== 'no_style') {
+      return (
+        <span className='info'>{this.props.styleOptions.getIdentifierLabel(Object.assign({}, this.props.styleOptions.defaults, this.props.entry.options))}</span>
+      );
+    }
+  }
+}
--- lib/shared/screens/admin/shared/components/page-builder-menu/tabs/style/style.jsx
+++ lib/shared/screens/admin/shared/components/page-builder-menu/tabs/style/style.jsx
@@ -1,10 +1,63 @@
+import getElementProps from 'helpers/get-element-props';
 import Component from 'components/component';
-import React from 'react';
+import React, {PropTypes} from 'react';
+
+import StylePicker from './style-picker';
 
 export default class Style extends Component {
+  static propTypes = {
+    selectedElement: PropTypes.object,
+    elements: PropTypes.object,
+    display: PropTypes.string,
+    styles: PropTypes.array.isRequired
+  };
+
   render () {
+    const {selectedElement, elements} = this.props;
+    let result;
+
+    if (selectedElement) {
+      const Element = elements[selectedElement.tag];
+
+      if (Element && Element.style) {
+        result = this.renderStylePicker(Element);
+      } else {
+        result = this.renderNotStylable();
+      }
+    } else {
+      result = this.renderNoneSelected();
+    }
+
+    return result;
+  }
+
+  renderStylePicker (Element) {
+    const {selectedElement, display} = this.props;
+    const elementProps = getElementProps(selectedElement, display);
+
     return (
-      <div>style</div>
+      <StylePicker
+        style={Element.style}
+        value={elementProps && elementProps.style}
+      />
+    );
+  }
+
+  renderNoneSelected () {
+    return (
+      <div>
+        <i className='material-icons'></i>
+        <div>No element selected, select an element first.</div>
+      </div>
+    );
+  }
+
+  renderNotStylable () {
+    return (
+      <div>
+        <i className='material-icons'></i>
+        <div>Current selected element has no style options, head to settings tab to edit its properties.</div>
+      </div>
     );
   }
 }
