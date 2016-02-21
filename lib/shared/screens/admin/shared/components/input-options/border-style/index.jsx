ddf27bcb174551940fa8cd097ae0c05b118f4074
Bruno Mota <bruno12mota@gmail.com>
2016-02-21 18:37:38 +0100

    border style input option

---
+++ lib/shared/screens/admin/shared/components/input-options/border-style/index.jsx
@@ -0,0 +1,32 @@
+import cx from 'classnames';
+import Component from 'components/component';
+import React from 'react';
+
+export default class BorderStyle extends Component {
+  static propTypes = {
+    value: React.PropTypes.string.isRequired,
+    onChange: React.PropTypes.func.isRequired
+  };
+
+  onClick (type, event) {
+    event.preventDefault();
+    this.props.onChange(type);
+  }
+
+  render () {
+    return (
+      <div className='border-style'>
+        {this.renderOption('solid')}
+        {this.renderOption('dashed')}
+        {this.renderOption('dotted')}
+        {this.renderOption('double')}
+      </div>
+    );
+  }
+
+  renderOption (type) {
+    return (
+      <div className={cx(type, this.props.value === type && 'active')} onClick={this.onClick.bind(this, type)} />
+    );
+  }
+}
