246b9ea9c2dc84f5b384dae1e465d647553cd657
Bruno Mota <bruno12mota@gmail.com>
2016-02-22 15:09:01 +0100

    page builder menu breadcrumbs

---
+++ lib/shared/screens/admin/shared/components/page-builder-menu/breadcrumbs/breadcrumbs.jsx
@@ -0,0 +1,32 @@
+import cx from 'classnames';
+import Component from 'components/component';
+import React, {PropTypes} from 'react';
+
+import styles from './breadcrumbs.less';
+import Entry from './entry';
+
+export default class Breadcrumbs extends Component {
+  static propTypes = {
+    selectElement: PropTypes.func.isRequired,
+    selectedPath: PropTypes.array.isRequired,
+    selectedElement: PropTypes.object,
+    className: PropTypes.string
+  };
+
+  render () {
+    const {selectedPath, selectedElement, className} = this.props;
+    return (
+      <div className={cx(styles.root, className)}>
+        {selectedPath && selectedPath.map(this.renderEntry, this)}
+        <span className={styles.current} key='current'>{(selectedElement && (selectedElement.label || selectedElement.tag)) || 'body'}</span>
+      </div>
+    );
+  }
+
+  renderEntry (entry) {
+    const {selectElement} = this.props;
+    return (
+      <Entry key={entry.id} entry={entry} onClick={selectElement} />
+    );
+  }
+}
