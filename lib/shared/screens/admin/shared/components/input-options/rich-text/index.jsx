4754d875673abe365de84110e6b3efd12bdba8c5
Bruno Mota <bruno12mota@gmail.com>
2016-02-22 10:49:51 +0100

    rich text input option

---
+++ lib/shared/screens/admin/shared/components/input-options/rich-text/index.jsx
@@ -0,0 +1,33 @@
+import classNames from 'classnames';
+import Component from 'components/component';
+import Editor from 'components/medium-editor';
+import React, {PropTypes} from 'react';
+
+export default class HtmlArea extends Component {
+  static propTypes = {
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    className: PropTypes.string
+  };
+
+  render () {
+    return (
+      <div className={classNames('html-area', this.props.className)}>
+        <Editor
+          tag='div'
+          className='html-editor'
+          value={this.props.value}
+          onChange={this.props.onChange}
+          options={{
+            toolbar: {
+              buttons: ['bold', 'italic', 'underline', 'anchor']
+            },
+            placeholder: false,
+            buttonLabels: 'fontawesome',
+            imageDragging: false
+          }}
+        />
+      </div>
+    );
+  }
+}
