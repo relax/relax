f01ec6d1785de40d257476953c0619b058e98b57
Bruno Mota <bruno12mota@gmail.com>
2016-02-25 18:41:24 +0100

    admin menu single route

---
+++ lib/shared/screens/admin/screens/menus/screens/menu/components/menu-builder.jsx
@@ -0,0 +1,10 @@
+import Component from 'components/component';
+import React from 'react';
+
+export default class MenuBuilder extends Component {
+  render () {
+    return (
+      <div>Builder</div>
+    );
+  }
+}
