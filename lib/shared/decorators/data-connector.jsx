c47d013fee9c284977d63faa780b597664bddf2a
Bruno Mota <bruno12mota@gmail.com>
2016-02-29 17:47:01 +0100

    graphql main store

--- lib/shared/decorators/data-connector.jsx
+++ lib/shared/decorators/data-connector.jsx
@@ -1,9 +1,22 @@
+import forEach from 'lodash.foreach';
 import hoistStatics from 'hoist-non-react-statics';
 import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
+
+var ID = 0;
 
 export default function dataConnect () {
   return function wrapWithDataConnect (WrappedComponent) {
+    @connect(
+      (state) => ({
+        graphql: state.graphql
+      })
+    )
     class ConnectData extends Component {
+      static propTypes = {
+        graphql: PropTypes.object.isRequired
+      };
+
       static contextTypes = {
         fetchData: PropTypes.func.isRequired
       };
@@ -14,23 +27,110 @@ export default function dataConnect () {
         this.state = {
           loading: true
         };
+        this.ID = 'connector_' + ID++;
+      }
+
+      shouldComponentUpdate (nextProps) {
+        const keysA = Object.keys(this.props);
+        const keysB = Object.keys(nextProps);
+
+        if (keysA.length !== keysB.length) {
+          return true;
+        }
+
+        const bHasOwnProperty = Object.prototype.hasOwnProperty.bind(nextProps);
+        let result = false;
+        forEach(keysA, (key) => {
+          if (!bHasOwnProperty(key)) {
+            result = true;
+            return false;
+          } else if (this.props[key] !== nextProps[key]) {
+            if (key === 'graphql') {
+              const prevGraphql = this.props.graphql;
+              const nextGraphql = nextProps.graphql;
+              const nextConnectorMap = nextGraphql[this.ID];
+
+              // Connector data map chaged
+              if (prevGraphql[this.ID] !== nextConnectorMap) {
+                result = true;
+                return false;
+              }
+
+              // check each one
+              let resultSearch = false;
+              forEach(nextConnectorMap, (data, queryName) => {
+                if (data.constructor === Array) {
+                  let resultIt = false;
+                  forEach(data, (dataEntry) => {
+                    if (prevGraphql[dataEntry] !== nextGraphql[dataEntry]) {
+                      resultIt = true;
+                      return false;
+                    }
+                  });
+                  if (resultIt) {
+                    resultSearch = true;
+                    return false;
+                  }
+                } else {
+                  if (prevGraphql[data] !== nextGraphql[data]) {
+                    resultSearch = true;
+                    return false;
+                  }
+                }
+              });
+
+              // some entry changed
+              if (resultSearch) {
+                result = true;
+                return false;
+              }
+            } else {
+              result = true;
+              return false;
+            }
+          }
+        });
+
+        return result;
       }
 
       childFetchData (data) {
         const {fetchData} = this.context;
 
         if (fetchData) {
-          fetchData(data).then(() => {
+          fetchData(data, this.ID).then(() => {
             this.setState({
               loading: false
             });
           });
-
         }
       }
 
       render () {
-        return <WrappedComponent {...this.props} fetchData={this.childFetchDataBind} loading={this.state.loading} />;
+        // get data needed from store
+        // check what data it is
+        const {graphql} = this.props;
+        const dataNeeds = graphql[this.ID];
+
+        // dataNeeds should have e.g.
+        // {
+        //   pages[query]: [id, id1, id2],
+        //   page[query]: id3
+        // }
+        const dataToInject = {};
+        forEach(dataNeeds, (data, queryName) => {
+          if (data.constructor === Array) {
+            dataToInject[queryName] = [];
+            forEach(data, (id) => {
+              dataToInject[queryName].push(graphql[id]);
+            });
+          } else {
+            dataToInject[queryName] = graphql[data];
+          }
+        });
+        console.log(dataToInject);
+
+        return <WrappedComponent {...this.props} {...dataToInject} fetchData={this.childFetchDataBind} loading={this.state.loading} />;
       }
     }
 
