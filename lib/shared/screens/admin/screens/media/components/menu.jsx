3683578740646c5fc4f236214a9e48f0635c10dd
Bruno Mota <bruno12mota@gmail.com>
2016-03-15 17:20:18 +0100

    admin media collapsable sub categories

--- lib/shared/screens/admin/screens/media/components/menu.jsx
+++ lib/shared/screens/admin/screens/media/components/menu.jsx
@@ -2,6 +2,7 @@ import Button from 'components/menu-button';
 import Component from 'components/component';
 import ListHeader from 'components/list-header';
 import Scrollable from 'components/scrollable';
+import SubButton from 'components/menu-sub-button';
 import React, {PropTypes} from 'react';
 
 import styles from './menu.less';
@@ -28,7 +29,11 @@ export default class MediaMenu extends Component {
         <Scrollable className={styles.list}>
           <div>
             <Button link='#' label='All' icon='nc-icon-outline files_single-copies' active />
-            <Button link='#' label='Images' icon='nc-icon-outline media-1_image-02' />
+            <Button link='#' label='Images' icon='nc-icon-outline media-1_image-02'>
+              <SubButton link='#' label='JPEG' />
+              <SubButton link='#' label='PNG' />
+              <SubButton link='#' label='ICO' />
+            </Button>
             <Button link='#' label='Video' icon='nc-icon-outline media-1_play-69' />
             <Button link='#' label='Audio' icon='nc-icon-outline media-1_volume-98' />
           </div>
