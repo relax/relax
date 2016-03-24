ad472d4d14e881b90ad6f3f8b3e29b64271bfad3
Bruno Mota <bruno12mota@gmail.com>
2016-03-24 20:02:36 +0000

    improved image component

--- lib/shared/components/image.jsx
+++ lib/shared/components/image.jsx
@@ -1,4 +1,3 @@
-import forEach from 'lodash.foreach';
 import utils from 'helpers/utils';
 import Component from 'components/component';
 import React, {PropTypes} from 'react';
@@ -6,39 +5,26 @@ import React, {PropTypes} from 'react';
 export default class Image extends Component {
   static propTypes = {
     id: PropTypes.string.isRequired,
-    width: PropTypes.number.isRequired,
-    height: PropTypes.number,
-    editing: PropTypes.bool
+    width: PropTypes.number,
+    height: PropTypes.number
+  };
+
+  static defaultProps = {
+    width: 200,
+    height: 0
   };
 
   render () {
-    if (this.props.id && this.props.id !== '') {
-      const url = utils.getBestImageUrl(this.props.id, this.props.width);
-      const extraProps = {};
+    const {id, width, height, ...htmlProps} = this.props;
+    let result = null;
 
-      forEach(this.props, (value, key) => {
-        if (key !== 'id' && key !== 'width' && key !== 'height') {
-          extraProps[key] = value;
-        }
-      });
-
-      return (
-        <img src={url} {...extraProps} />
-      );
-    } else if (this.props.editing) {
-      const style = {};
-
-      if (this.props.height) {
-        style.height = this.props.height;
-      }
-
-      return (
-        <div className='dummy-placeholder' style={style}>
-          <i className='fa fa-image'></i>
-        </div>
+    if (id) {
+      const url = utils.getBestImageUrl(id, width, height);
+      result = (
+        <img src={url} {...htmlProps} />
       );
     }
 
-    return null;
+    return result;
   }
 }
