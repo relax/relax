65ecc720180c787961cb43051ae0fa1e0fc9c891
Bruno Mota <bruno12mota@gmail.com>
2016-03-24 20:03:26 +0000

    uploading styling

--- lib/shared/screens/admin/screens/media/components/uploading/uploading.jsx
+++ lib/shared/screens/admin/screens/media/components/uploading/uploading.jsx
@@ -1,4 +1,7 @@
+import velocity from 'velocity-animate';
+import Animate from 'components/animate';
 import Component from 'components/component';
+import Scrollable from 'components/scrollable';
 import React, {PropTypes} from 'react';
 
 import styles from './uploading.less';
@@ -6,18 +9,50 @@ import Item from './item';
 
 export default class Uploading extends Component {
   static propTypes = {
-    uploads: PropTypes.array.isRequired
+    uploads: PropTypes.array.isRequired,
+    opened: PropTypes.bool.isRequired,
+    toggleOpened: PropTypes.func.isRequired
   };
 
+  componentWillReceiveProps (nextProps) {
+    if (this.props.opened !== nextProps.opened) {
+      const config = {
+        duration: 500,
+        easing: 'easeOutExpo'
+      };
+
+      if (nextProps.opened) {
+        velocity(this.refs.root, {translateY: '0%'}, config);
+      } else {
+        velocity(this.refs.root, {translateY: '100%'}, config);
+      }
+    }
+  }
+
   render () {
-    const {uploads} = this.props;
+    const {uploads, toggleOpened} = this.props;
+    const uploadsFinished = uploads.reduce((previousValue, current) => {
+      const isFinished = current.status !== 'queue' && current.status !== 'uploading';
+      return previousValue + (isFinished ? 1 : 0);
+    }, 0);
     return (
-      <div className={styles.root}>
-        <div className={styles.title}>Uploads</div>
-        <div className={styles.list}>
-          {uploads.map(this.renderUpload, this)}
+      <Animate>
+        <div className={styles.root} ref='root'>
+          <div className={styles.header} onClick={toggleOpened}>
+            <span className={styles.title}>Uploads</span>
+            <span className={styles.progress}>
+              <span>{uploadsFinished}</span>
+              <span> / </span>
+              <span>{uploads.length}</span>
+            </span>
+          </div>
+          <Scrollable className={styles.list}>
+            <div className={styles.listContent}>
+              {uploads.map(this.renderUpload, this)}
+            </div>
+          </Scrollable>
         </div>
-      </div>
+      </Animate>
     );
   }
 
