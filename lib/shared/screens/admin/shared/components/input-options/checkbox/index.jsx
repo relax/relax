6543801f22f351c75d5a12d89c9261d7f576c942
Bruno Mota <bruno12mota@gmail.com>
2016-04-07 14:50:51 +0100

    schema module builder

--- lib/shared/screens/admin/shared/components/input-options/checkbox/index.jsx
+++ lib/shared/screens/admin/shared/components/input-options/checkbox/index.jsx
@@ -1,3 +1,4 @@
+import bind from 'decorators/bind';
 import cx from 'classnames';
 import Component from 'components/component';
 import React from 'react';
@@ -8,21 +9,31 @@ export default class Checkbox extends Component {
   static propTypes = {
     value: React.PropTypes.bool.isRequired,
     onChange: React.PropTypes.func.isRequired,
-    disabled: React.PropTypes.bool
+    disabled: React.PropTypes.bool,
+    white: React.PropTypes.bool
   };
 
+  @bind
   toggle (event) {
     event.preventDefault();
+    const {disabled, onChange, value} = this.props;
 
-    if (!this.props.disabled && this.props.onChange) {
-      this.props.onChange(!this.props.value);
+    if (!disabled && onChange) {
+      onChange(!value);
     }
   }
 
   render () {
-    const {disabled, value} = this.props;
+    const {disabled, value, white} = this.props;
     return (
-      <span className={cx(styles.checkbox, disabled && styles.disabled)} onClick={::this.toggle}>
+      <span
+        className={cx(
+          styles.checkbox,
+          white && styles.white,
+          disabled && styles.disabled
+        )}
+        onClick={this.toggle}
+      >
         {value && <i className='nc-icon-mini ui-1_check'></i>}
       </span>
     );
