b8fe36f622b480b6476587b44c606bd185bc0f9d
Bruno Mota <bruno12mota@gmail.com>
2016-04-08 12:02:44 +0100

    menu active schema page

--- lib/shared/screens/admin/components/menu/content-types.jsx
+++ lib/shared/screens/admin/components/menu/content-types.jsx
@@ -16,6 +16,8 @@ export default class ContentTypes extends Component {
   };
 
   static propTypes = {
+    active: PropTypes.string,
+    onActiveClick: PropTypes.func.isRequired,
     schemas: PropTypes.array.isRequired
   };
 
@@ -35,6 +37,7 @@ export default class ContentTypes extends Component {
   }
 
   renderSchema (schema) {
+    const {active, onActiveClick} = this.props;
     return (
       <Button
         link={`/admin/schemas/${schema._id}`}
@@ -43,6 +46,8 @@ export default class ContentTypes extends Component {
           'nc-icon-outline',
           schema.type === 'single' ? 'design_webpage' : 'files_single-copy-04'
         )}
+        active={active === schema._id}
+        onActiveClick={onActiveClick}
         dark
       />
     );
