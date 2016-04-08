fc2c1e3b27debcec36b3a6e2d4d00587d25d32ff
Bruno Mota <bruno12mota@gmail.com>
2016-04-08 15:07:58 +0100

    selected schema entry page states ui

--- lib/shared/screens/admin/screens/schemas/screens/schema/components/schema.jsx
+++ lib/shared/screens/admin/screens/schemas/screens/schema/components/schema.jsx
@@ -1,14 +1,49 @@
 import Component from 'components/component';
 import React, {PropTypes} from 'react';
 
+import styles from './schema.less';
+import New from './new';
+
 export default class Schema extends Component {
   static propTypes = {
-    children: PropTypes.node
+    children: PropTypes.node,
+    count: PropTypes.number.isRequired
   };
 
   render () {
+    let result;
+
+    if (this.props.count === 0) {
+      result = this.renderNoPages();
+    } else if (this.props.children) {
+      result = this.props.children;
+    } else {
+      result = this.renderEmpty();
+    }
+
+    return result;
+  }
+
+  renderEmpty () {
     return (
-      <div>Schema</div>
+      <div className={styles.empty}>
+        <i className='nc-icon-outline media-1_touch'></i>
+        <div className={styles.emptyText}>Relax, select an entry first!</div>
+      </div>
+    );
+  }
+
+  renderNoPages () {
+    return (
+      <div className={styles.noEntries}>
+        <div className={styles.noTitle}>Oh my!</div>
+        <div className={styles.noText}>
+          <span>You don’t have any entries yet!</span>
+          <br />
+          <span>Lets change that</span>
+        </div>
+        <New />
+      </div>
     );
   }
 }
