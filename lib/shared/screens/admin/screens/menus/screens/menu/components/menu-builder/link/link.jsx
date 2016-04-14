625424e8c692d62dfc5d1465ed7d8af3d79d374d
Bruno Mota <bruno12mota@gmail.com>
2016-04-14 12:36:38 +0100

    menu builder pages and custom link

---
+++ lib/shared/screens/admin/screens/menus/screens/menu/components/menu-builder/link/link.jsx
@@ -0,0 +1,32 @@
+import Component from 'components/component';
+import Input from 'components/input-options/input';
+import React, {PropTypes} from 'react';
+
+import styles from './link.less';
+import Entry from '../entry';
+
+export default class Link extends Component {
+  static propTypes = {
+    label: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    changeLabel: PropTypes.func.isRequired,
+    changeUrl: PropTypes.func.isRequired
+  };
+
+  render () {
+    const {changeLabel, changeUrl, label, url} = this.props;
+    return (
+      <div>
+        <label className={styles.option}>
+          <div className={styles.label}>Label</div>
+          <Input value={label} onChange={changeLabel} white />
+        </label>
+        <label className={styles.option}>
+          <div className={styles.label}>Link</div>
+          <Input value={url} onChange={changeUrl} white />
+        </label>
+        <Entry label={label} type='url' />
+      </div>
+    );
+  }
+}
