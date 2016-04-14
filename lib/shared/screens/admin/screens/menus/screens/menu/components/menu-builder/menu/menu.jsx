4ddca915843eed78490e6dfa44255ee7830bdec9
Bruno Mota <bruno12mota@gmail.com>
2016-04-14 16:16:07 +0100

    menu dropping state

--- lib/shared/screens/admin/screens/menus/screens/menu/components/menu-builder/menu/menu.jsx
+++ lib/shared/screens/admin/screens/menus/screens/menu/components/menu-builder/menu/menu.jsx
@@ -1,4 +1,6 @@
 import bind from 'decorators/bind';
+import cx from 'classnames';
+import Animate from 'components/animate';
 import Component from 'components/component';
 import Dragger from 'components/dnd/dragger';
 import Droppable from 'components/dnd/droppable';
@@ -24,26 +26,47 @@ export default class Menu extends Component {
         <Droppable
           placeholder
           placeholderRender={this.renderPlaceholder}
-          dropInfo={{}}
+          dropInfo={{
+            id: 'root'
+          }}
           minHeight='100%'
-        >
-        </Droppable>
+        />
         {this.renderDragger()}
       </div>
     );
   }
 
   @bind
-  renderPlaceholder () {
+  renderPlaceholder ({isActive}) {
+    let placeholderContent;
+
+    if (isActive) {
+      placeholderContent = (
+        <Animate transition='slideDownIn' key='drop'>
+          <i className={cx('nc-icon-outline weather_drop-12', styles.dropIcon)} />
+        </Animate>
+      );
+    } else {
+      placeholderContent = (
+        <Animate transition='slideUpIn' key='info'>
+          <div>
+            <div className={styles.title}>Drop your links here!</div>
+            <div className={styles.text}>
+              Drag pages or custom links from the left and drop them in this
+              section to start building your menu structure.
+            </div>
+            <div className={styles.text}>
+              Remember you can also sort them by moving them above or bellow other items.
+            </div>
+          </div>
+        </Animate>
+      );
+    }
+
     return (
-      <div className={styles.empty}>
-        <div className={styles.title}>Drop your links here!</div>
-        <div className={styles.text}>
-          Drag pages or custom links from the left and drop them in this
-          section to start building your menu structure.
-        </div>
-        <div className={styles.text}>
-          Remember you can also sort them by moving them above or bellow other items.
+      <div className={cx(styles.empty, isActive && styles.active)}>
+        <div className={styles.emptyContent}>
+          {placeholderContent}
         </div>
       </div>
     );
