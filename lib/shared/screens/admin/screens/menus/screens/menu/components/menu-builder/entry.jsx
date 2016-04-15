4632c533b6fa0fd4582702f2a2debe4d2eb47165
Bruno Mota <bruno12mota@gmail.com>
2016-04-15 09:31:53 +0100

    menu builder sub items

--- lib/shared/screens/admin/screens/menus/screens/menu/components/menu-builder/entry.jsx
+++ lib/shared/screens/admin/screens/menus/screens/menu/components/menu-builder/entry.jsx
@@ -1,13 +1,19 @@
+import bind from 'decorators/bind';
 import cx from 'classnames';
+import Animate from 'components/animate';
 import Component from 'components/component';
 import Draggable from 'components/dnd/draggable';
+import Droppable from 'components/dnd/droppable';
 import React, {PropTypes} from 'react';
 
 import styles from './entry.less';
 
 export default class MenuEntry extends Component {
   static propTypes = {
-    item: PropTypes.object.isRequired
+    item: PropTypes.object.isRequired,
+    children: PropTypes.node,
+    dragging: PropTypes.bool,
+    draggedMenuItem: PropTypes.func
   };
 
   static defaultProps = {
@@ -17,18 +23,65 @@ export default class MenuEntry extends Component {
   render () {
     const {item} = this.props;
     const {label, type} = item;
+    const isNew = !(item.id && true);
 
     const dragInfo = {
-      type: 'new',
+      type: isNew ? 'new' : 'move',
       item
     };
 
     return (
       <Draggable dragInfo={dragInfo}>
-        <div className={cx(styles.root, styles[type])}>
-          {label || type === 'url' && 'Where Link appears to be dragged'}
+        <div className={styles.root}>
+          <div className={cx(styles.info, styles[type])}>
+            {label || type === 'url' && isNew && 'Where Link appears to be dragged'}
+          </div>
+          {!isNew && this.renderChildren()}
         </div>
       </Draggable>
+    );
+  }
+
+  renderChildren () {
+    const {item, dragging} = this.props;
+    return (
+      <Droppable
+        className={styles.sub}
+        dropInfo={{
+          id: item.id
+        }}
+        placeholder={dragging}
+        placeholderRender={this.renderPlaceholder}
+        hidePlaceholder
+        minHeight={dragging ? 30 : 0}
+      >
+        {this.props.children}
+      </Droppable>
+    );
+  }
+
+  @bind
+  renderPlaceholder ({isActive}) {
+    let placeholderContent;
+
+    if (isActive) {
+      placeholderContent = (
+        <Animate transition='slideDownIn' key='drop'>
+          <i className={cx('nc-icon-outline weather_drop-12', styles.dropIcon)} />
+        </Animate>
+      );
+    } else {
+      placeholderContent = (
+        <Animate transition='slideUpIn' key='info'>
+          <div className={styles.emptyText}>Drop here for sub link!</div>
+        </Animate>
+      );
+    }
+
+    return (
+      <div className={cx(styles.empty, isActive && styles.active)}>
+        {placeholderContent}
+      </div>
     );
   }
 }
