5accadcd012b686ff3b54c5105c641a8ea9f52fc
Bruno Mota <bruno12mota@gmail.com>
2016-04-15 17:53:53 +0100

    admin settings menu

---
+++ lib/shared/screens/admin/screens/settings/screens/general/components/general.jsx
@@ -0,0 +1,12 @@
+import Component from 'components/component';
+import React, {PropTypes} from 'react';
+
+export default class GeneralSettings extends Component {
+  static propTypes = {};
+
+  render () {
+    return (
+      <div>Settings</div>
+    );
+  }
+}
