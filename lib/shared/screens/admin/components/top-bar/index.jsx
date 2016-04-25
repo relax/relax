e80acbc0fdd7a7cbf772f56133a707f14bcde7f8
Bruno Mota <bruno12mota@gmail.com>
2016-04-25 21:16:03 +0100

    page builder toggle preview

--- lib/shared/screens/admin/components/top-bar/index.jsx
+++ lib/shared/screens/admin/components/top-bar/index.jsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import velocity from 'velocity-animate';
 import Component from 'components/component';
+import React, {PropTypes} from 'react';
 
 import styles from './index.less';
 import Actions from './actions';
 import Tabs from './tabs';
 
 export default class TopBar extends Component {
+  static propTypes = {
+    previewing: PropTypes.bool.isRequired,
+    toggleEditing: PropTypes.func.isRequired
+  };
+
+  componentWillReceiveProps (nextProps) {
+    const config = {
+      duration: 800,
+      display: null,
+      easing: 'easeOutExpo'
+    };
+
+    if (nextProps.previewing !== this.props.previewing) {
+      if (nextProps.previewing) {
+        velocity(this.refs.content, {translateY: '-45px'}, config);
+        velocity(this.refs.preview, {translateX: '-110px'}, config);
+      } else {
+        velocity(this.refs.content, {translateY: '0'}, config);
+        velocity(this.refs.preview, {translateX: '-0'}, config);
+      }
+    }
+  }
+
   render () {
+    const {toggleEditing} = this.props;
     return (
-      <div className={styles.root}>
-        <Actions />
-        <Tabs />
+      <div>
+        <div className={styles.root} ref='content'>
+          <Actions />
+          <Tabs />
+        </div>
+        <div className={styles.closePreview} ref='preview' onClick={toggleEditing}>
+          Close preview
+        </div>
       </div>
     );
   }
