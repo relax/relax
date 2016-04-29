94d5c2a4cb2ad7e70b27075a68eaf7b654a7eba4
Bruno Mota <bruno12mota@gmail.com>
2016-04-29 14:47:49 +0100

    admin menu user opened actions

--- lib/shared/screens/admin/components/menu/user.jsx
+++ lib/shared/screens/admin/components/menu/user.jsx
@@ -1,7 +1,9 @@
+import bind from 'decorators/bind';
 import cx from 'classnames';
 import getGravatarImage from 'helpers/get-gravatar-image';
 import Component from 'components/component';
 import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
 
 import styles from './user.less';
 
@@ -24,6 +26,7 @@ export default class User extends Component {
     };
   }
 
+  @bind
   toggleOpened () {
     this.setState({
       opened: !this.state.opened
@@ -36,22 +39,38 @@ export default class User extends Component {
     const url = getGravatarImage(email, 50);
 
     return (
-      <div className={styles.root}>
-        <div className={styles.thumbnail}>
-          <div className={styles.blurred}>
-            <img src={url} role='presentation' />
+      <div className={cx(styles.root, opened && styles.opened)}>
+        <div className={styles.head} onClick={this.toggleOpened}>
+          <div className={styles.thumbnail}>
+            <div className={styles.blurred}>
+              <img src={url} role='presentation' />
+            </div>
+            <div className={styles.image}>
+              <img src={url} role='presentation' />
+            </div>
           </div>
-          <div className={styles.image}>
-            <img src={url} role='presentation' />
+          <div className={styles.info}>
+            <div className={styles.name}>{name}</div>
+            <div className={styles.role}>Administrator</div>
+          </div>
+          <div className={styles.toggle}>
+            <i className={cx('nc-icon-mini', opened ? 'arrows-1_minimal-up' : 'arrows-1_minimal-down')}></i>
           </div>
         </div>
-        <div className={styles.info}>
-          <div className={styles.name}>{name}</div>
-          <div className={styles.role}>Administrator</div>
+        <div className={styles.actions}>
+          <Link to='/admin/users' className={styles.action}>
+            <i className='nc-icon-mini users_circle-08'></i>
+            <span>My Profile</span>
+          </Link>
+          <a href='https://github.com/relax/relax/issues' className={styles.action}>
+            <i className='nc-icon-mini design_bug'></i>
+            <span>Help</span>
+          </a>
+          <a href='/admin/logout' className={styles.action}>
+            <i className='nc-icon-mini arrows-1_log-out'></i>
+            <span>Logout</span>
+          </a>
         </div>
-        <button className={styles.toggle} onClick={::this.toggleOpened}>
-          <i className={cx('nc-icon-mini', opened ? 'arrows-1_minimal-up' : 'arrows-1_minimal-down')}></i>
-        </button>
       </div>
     );
   }
