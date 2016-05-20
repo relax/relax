cb69b468aed7634ffc9074c5cb25cd8f584f390a
Bruno Mota <bruno12mota@gmail.com>
2016-05-20 16:48:40 +0100

    seperate routes for single and data schema types

---
+++ lib/shared/screens/admin/screens/schemas/screens/data/shared/components/data-form/form.jsx
@@ -0,0 +1,10 @@
+import Component from 'components/component';
+import React from 'react';
+
+export default class DataSchemaForm extends Component {
+  render () {
+    return (
+      <div>Form goes here</div>
+    );
+  }
+}
