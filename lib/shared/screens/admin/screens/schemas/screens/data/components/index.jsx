0fb1bc210748f798be8957e073b87ec9ec5ff3d1
Bruno Mota <bruno12mota@gmail.com>
2016-06-07 23:51:11 +0200

    admin data schema types screen wireframe

--- lib/shared/screens/admin/screens/schemas/screens/data/components/index.jsx
+++ lib/shared/screens/admin/screens/schemas/screens/data/components/index.jsx
@@ -1,10 +1,57 @@
 import Component from 'components/component';
-import React from 'react';
+import Content from 'components/content';
+import ContentHeader from 'components/content-header';
+import ContentLoading from 'components/content-loading';
+import EditableTitle from 'components/editable-title';
+import React, {PropTypes} from 'react';
 
 export default class DataSchema extends Component {
+  static fragments = {
+    schema: {
+      _id: 1,
+      title: 1
+    }
+  };
+
+  static propTypes = {
+    schema: PropTypes.object,
+    loading: PropTypes.bool.isRequired
+  };
+
   render () {
+    const {schema, loading} = this.props;
+    let result;
+
+    if (loading) {
+      result = this.renderLoading();
+    } else if (!schema) {
+      result = this.renderNotFound();
+    } else {
+      result = this.renderContent();
+    }
+
+    return result;
+  }
+
+  renderLoading () {
     return (
-      <div>Data Schema list goes here</div>
+      <ContentLoading />
     );
+  }
+
+  renderContent () {
+    const {schema} = this.props;
+    return (
+      <div>
+        <ContentHeader>
+          <EditableTitle value={schema.title} onSubmit={() => {}} />
+        </ContentHeader>
+        <Content></Content>
+      </div>
+    );
+  }
+
+  renderNotFound () {
+    return (<div>Schema not found</div>);
   }
 }
