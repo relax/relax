73dda78c3f21e576a9b00953aea729e701e492b7
Bruno Mota <bruno12mota@gmail.com>
2016-06-16 17:56:48 +0200

    icon selector redone

---
+++ lib/shared/screens/admin/shared/components/input-options/icon/icon-selector/icon.jsx
@@ -0,0 +1,35 @@
+import bind from 'decorators/bind';
+import cx from 'classnames';
+import Component from 'components/component';
+import React, {PropTypes} from 'react';
+
+import styles from './icon.less';
+
+export default class Icon extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    selected: PropTypes.bool,
+    icon: PropTypes.string,
+    children: PropTypes.node,
+    onClick: PropTypes.func.isRequired
+  };
+
+  @bind
+  onClick () {
+    const {onClick, icon} = this.props;
+    onClick(icon);
+  }
+
+  render () {
+    const {className, children, icon, selected} = this.props;
+    return (
+      <div
+        className={cx(styles.root, selected && styles.selected)}
+        onClick={this.onClick}
+      >
+        <i className={className}>{children}</i>
+        <div className={styles.label}>{icon}</div>
+      </div>
+    );
+  }
+}
