93f4cf5e41b99a4762bf8846001bbddd7feb5811
Bruno Mota <bruno12mota@gmail.com>
2016-06-23 18:29:25 +0100

    options listing improvements

--- lib/shared/screens/admin/screens/schemas/screens/data/shared/components/data-form/form.jsx
+++ lib/shared/screens/admin/screens/schemas/screens/data/shared/components/data-form/form.jsx
@@ -1,10 +1,37 @@
 import Component from 'components/component';
-import React from 'react';
+import ContentLoading from 'components/content-loading';
+import OptionsList from 'components/options-list';
+import React, {PropTypes} from 'react';
 
 export default class DataSchemaForm extends Component {
+  static propTypes = {
+    loading: PropTypes.bool,
+    properties: PropTypes.array
+  };
+
   render () {
+    const {loading} = this.props;
+    let result;
+
+    if (loading) {
+      result = this.renderLoading();
+    } else {
+      result = this.renderForm();
+    }
+
+    return result;
+  }
+
+  renderLoading () {
     return (
-      <div>Form goes here</div>
+      <ContentLoading />
+    );
+  }
+
+  renderForm () {
+    const {properties} = this.props;
+    return (
+      <OptionsList options={properties} values={{}} white />
     );
   }
 }
