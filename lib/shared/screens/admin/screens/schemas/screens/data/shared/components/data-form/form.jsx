3f709060ec610ce5fc845a71b8373b38f37ab60c
Bruno Mota <bruno12mota@gmail.com>
2016-07-01 16:56:26 +0100

    new schema entry back to list

--- lib/shared/screens/admin/screens/schemas/screens/data/shared/components/data-form/form.jsx
+++ lib/shared/screens/admin/screens/schemas/screens/data/shared/components/data-form/form.jsx
@@ -1,10 +1,16 @@
 import Component from 'components/component';
+import Content from 'components/content';
+import ContentHeader from 'components/content-header';
 import ContentLoading from 'components/content-loading';
 import OptionsList from 'components/options-list';
 import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
+
+import styles from './form.less';
 
 export default class DataSchemaForm extends Component {
   static propTypes = {
+    schemaId: PropTypes.string.isRequired,
     loading: PropTypes.bool,
     properties: PropTypes.array,
     values: PropTypes.object.isRequired,
@@ -31,14 +37,27 @@ export default class DataSchemaForm extends Component {
   }
 
   renderForm () {
-    const {properties, onChange, values} = this.props;
+    const {properties, onChange, values, schemaId} = this.props;
     return (
-      <OptionsList
-        options={properties}
-        values={values}
-        onChange={onChange}
-        white
-      />
+      <div>
+        <ContentHeader>
+          <Link
+            to={`/admin/schemas/data/${schemaId}`}
+            className={styles.back}
+          >
+            <i className='nc-icon-outline arrows-1_small-triangle-left' />
+            <span>Back to list</span>
+          </Link>
+        </ContentHeader>
+        <Content>
+          <OptionsList
+            options={properties}
+            values={values}
+            onChange={onChange}
+            white
+          />
+        </Content>
+      </div>
     );
   }
 }
