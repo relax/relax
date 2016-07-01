2a42043d72990d30413fed5dec9969cebe65cccb
Bruno Mota <bruno12mota@gmail.com>
2016-07-01 18:15:24 +0100

    create schema entry action

--- lib/shared/screens/admin/screens/schemas/screens/data/shared/components/data-form/form.jsx
+++ lib/shared/screens/admin/screens/schemas/screens/data/shared/components/data-form/form.jsx
@@ -1,8 +1,12 @@
+import cx from 'classnames';
+import Animate from 'components/animate';
+import Button from 'components/button';
 import Component from 'components/component';
 import Content from 'components/content';
 import ContentHeader from 'components/content-header';
 import ContentLoading from 'components/content-loading';
 import OptionsList from 'components/options-list';
+import Spinner from 'components/spinner';
 import React, {PropTypes} from 'react';
 import {Link} from 'react-router';
 
@@ -14,16 +18,19 @@ export default class DataSchemaForm extends Component {
     loading: PropTypes.bool,
     properties: PropTypes.array,
     values: PropTypes.object.isRequired,
-    onChange: PropTypes.func.isRequired
+    onChange: PropTypes.func.isRequired,
+    saving: PropTypes.bool,
+    saved: PropTypes.bool,
+    onSubmit: PropTypes.func.isRequired
   };
 
   render () {
-    const {loading} = this.props;
+    const {loading, properties} = this.props;
     let result;
 
     if (loading) {
       result = this.renderLoading();
-    } else {
+    } else if (properties) {
       result = this.renderForm();
     }
 
@@ -56,8 +63,44 @@ export default class DataSchemaForm extends Component {
             onChange={onChange}
             white
           />
+          <div className={styles.state}>
+            {this.renderState()}
+          </div>
         </Content>
       </div>
     );
+  }
+
+  renderState () {
+    const {saving, saved, onSubmit} = this.props;
+    let result;
+
+    if (saving) {
+      result = (
+        <Animate key='saving'>
+          <div>
+            <Spinner />
+            <span className={styles.savingLabel}>Creating</span>
+          </div>
+        </Animate>
+      );
+    } else if (saved) {
+      result = (
+        <Animate key='saved'>
+          <div>
+            <i className={cx(styles.success, 'nc-icon-mini ui-1_check')} />
+            <span className={cx(styles.success, styles.successLabel)}>Saved successfuly</span>
+          </div>
+        </Animate>
+      );
+    } else {
+      result = (
+        <Animate key='button'>
+          <Button primary onClick={onSubmit}>Create entry</Button>
+        </Animate>
+      );
+    }
+
+    return result;
   }
 }
