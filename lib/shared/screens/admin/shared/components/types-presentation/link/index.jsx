c72a6957b25e0b7ed92f1ce7e798eb05b0645455
Bruno Mota <bruno12mota@gmail.com>
2016-07-03 15:46:29 +0100

    data schema list presentation types

---
+++ lib/shared/screens/admin/shared/components/types-presentation/link/index.jsx
@@ -0,0 +1,66 @@
+import Component from 'components/component';
+import React, {PropTypes} from 'react';
+
+import styles from './index.less';
+
+export default class LinkType extends Component {
+  static propTypes = {
+    value: PropTypes.object
+  };
+
+  render () {
+    const {value} = this.props;
+    let result;
+
+    if (!value || !value.type) {
+      result = this.renderEmpty();
+    } else {
+      switch (value.type) {
+        case 'external':
+          result = this.renderExternal();
+          break;
+        case 'internal':
+          result = this.renderInternal();
+          break;
+        case 'anchor':
+          result = this.renderAnchor();
+          break;
+        default:
+          result = this.renderEmpty();
+      }
+    }
+
+    return result;
+  }
+
+  renderEmpty () {
+    return (
+      <div>-</div>
+    );
+  }
+
+  renderExternal () {
+    const {value} = this.props;
+    const options = value.options || {};
+
+    return (
+      <a href={options.url} className={styles.link}>
+        {options.url}
+      </a>
+    );
+  }
+
+  renderInternal () {
+    // TODO
+    return (
+      <span className={styles.link}>Internal</span>
+    );
+  }
+
+  renderAnchor () {
+    // TODO
+    return (
+      <span className={styles.link}>Internal</span>
+    );
+  }
+}
