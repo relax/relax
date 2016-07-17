f1101fe80300a4675f76cb65759417d633173d3b
Bruno Mota <bruno12mota@gmail.com>
2016-07-17 14:12:26 +0100

    dynamic list element expose default props

---
+++ lib/shared/elements/dynamic-list/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import propsSchema from './props-schema';
+import settings from './settings';
+import Component from '../component';
+import Container from './container';
+
+export default class DynamicListElement extends Component {
+  static defaultProps = {
+    limit: 10,
+    columns: 2,
+    verticalGutter: '10px',
+    horizontalGutter: '10px'
+  };
+
+  static propsSchema = propsSchema;
+  static settings = settings;
+
+  render () {
+    return (
+      <Container {...this.props} />
+    );
+  }
+}
