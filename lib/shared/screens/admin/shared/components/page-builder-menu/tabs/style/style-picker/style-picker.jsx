f831687509507609cfbb637d252a1f1326117df7
Bruno Mota <bruno12mota@gmail.com>
2016-09-17 17:45:42 +0100

    page builder change style title action

--- lib/shared/screens/admin/shared/components/page-builder-menu/tabs/style/style-picker/style-picker.jsx
+++ lib/shared/screens/admin/shared/components/page-builder-menu/tabs/style/style-picker/style-picker.jsx
@@ -1,31 +1,30 @@
-import cx from 'classnames';
 import Component from 'components/component';
 import React, {PropTypes} from 'react';
 
 import styles from './style-picker.less';
 import Editing from './editing';
 import List from './list';
+import Selected from './selected';
 
 export default class StylePicker extends Component {
   static propTypes = {
     editing: PropTypes.bool.isRequired,
     toggleEditing: PropTypes.func.isRequired,
-    selectedStyle: PropTypes.object
+    selectedStyle: PropTypes.object,
+    onTitleChange: PropTypes.func.isRequired
   };
 
   render () {
-    const {editing, toggleEditing, selectedStyle} = this.props;
+    const {editing, toggleEditing, onTitleChange, selectedStyle} = this.props;
 
     return (
       <div>
-        <div className={styles.selected} onClick={toggleEditing}>
-          <span>{selectedStyle && selectedStyle.title || 'No style'}</span>
-          <i className={cx(
-              'nc-icon-outline',
-              editing ? 'arrows-1_minimal-up' : 'arrows-1_minimal-down'
-            )}
-          />
-        </div>
+        <Selected
+          selectedStyle={selectedStyle}
+          onClick={toggleEditing}
+          onSubmit={onTitleChange}
+          opened={editing}
+        />
         <div className={styles.content}>
           {this.renderContent()}
         </div>
