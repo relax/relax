1797fa1f183dc38f73434f5373b393173e4cd342
Bruno Mota <bruno12mota@gmail.com>
2016-09-23 13:43:49 +0100

    new raw element with custom properties input option fixes #272

---
+++ lib/shared/screens/admin/shared/components/input-options/custom/entry.jsx
@@ -0,0 +1,59 @@
+import React, {PropTypes} from 'react';
+import Component from 'components/component';
+import Input from 'components/input-options/input';
+import bind from 'decorators/bind';
+import styles from './entry.less';
+import cx from 'classnames';
+
+export default class CustomInputOptionEntry extends Component {
+  static propTypes = {
+    property: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    onRemove: PropTypes.func.isRequired,
+    index: PropTypes.number.isRequired
+  };
+
+  @bind
+  onPropertyChange (property) {
+    const {onChange, value, index} = this.props;
+    onChange(index, {
+      value,
+      property
+    });
+  }
+
+  @bind
+  onValueChange (value) {
+    const {onChange, property, index} = this.props;
+    onChange(index, {
+      value,
+      property
+    });
+  }
+
+  @bind
+  onRemove () {
+    const {onRemove, index} = this.props;
+    onRemove(index);
+  }
+
+  render () {
+    const {property, value} = this.props;
+
+    return (
+      <div className={styles.root}>
+        <Input value={property} onChange={this.onPropertyChange} className={styles.input} />
+        <span className={styles.sep}>:</span>
+        <Input value={value} onChange={this.onValueChange} className={styles.input} />
+        <i
+          className={cx(
+            styles.removeButton,
+            'nc-icon-mini ui-1_trash-simple'
+          )}
+          onClick={this.onRemove}
+        />
+      </div>
+    );
+  }
+}
---
+++ lib/shared/screens/admin/shared/components/input-options/custom/index.jsx
@@ -0,0 +1,70 @@
+import React, {PropTypes} from 'react';
+import Component from 'components/component';
+import styles from './index.less';
+import Entry from './entry';
+import bind from 'decorators/bind';
+
+export default class CustomInputOption extends Component {
+  static propTypes = {
+    value: PropTypes.array,
+    onChange: PropTypes.func.isRequired
+  };
+
+  @bind
+  onPropertyChange (index, newProp) {
+    const {value = [], onChange} = this.props;
+    const newValue = value.slice(0);
+
+    newValue.splice(index, 1, newProp);
+    onChange(newValue);
+  }
+
+  @bind
+  addProperty () {
+    const {onChange, value = []} = this.props;
+    const newValue = value.slice(0);
+
+    newValue.push({
+      property: '',
+      value: ''
+    });
+    onChange(newValue);
+  }
+
+  @bind
+  onRemove (index) {
+    const {onChange, value = []} = this.props;
+    const newValue = value.slice(0);
+
+    newValue.splice(index, 1);
+    onChange(newValue);
+  }
+
+  render () {
+    const {value = []} = this.props;
+
+    return (
+      <div>
+        <div>
+          {value.map(this.renderProperty, this)}
+        </div>
+        <div className={styles.addButton} onClick={this.addProperty}>
+          Add property
+        </div>
+      </div>
+    );
+  }
+
+  renderProperty (prop, index) {
+    return (
+      <Entry
+        key={index}
+        index={index}
+        property={prop.property}
+        value={prop.value}
+        onChange={this.onPropertyChange}
+        onRemove={this.onRemove}
+      />
+    );
+  }
+}
