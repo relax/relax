8f5d1fe7e3ab013a8cec885fb58650d34c790cf8
Bruno Mota <bruno12mota@gmail.com>
2016-09-24 17:59:39 +0100

    background layers input option

---
+++ lib/shared/screens/admin/shared/components/input-options/background/entry.jsx
@@ -0,0 +1,137 @@
+import bind from 'decorators/bind';
+import cx from 'classnames';
+import utils from 'helpers/utils';
+import Balloon from 'components/balloon';
+import ColorPicker from 'components/input-options/color';
+import Component from 'components/component';
+import React, {PropTypes} from 'react';
+
+import styles from './entry.less';
+import EditImage from './edit-image';
+
+export default class BackgroundEntry extends Component {
+  static propTypes = {
+    type: PropTypes.oneOf(['color', 'image']).isRequired,
+    value: PropTypes.object.isRequired,
+    index: PropTypes.number.isRequired,
+    opened: PropTypes.bool,
+    onChange: PropTypes.func.isRequired,
+    onRemove: PropTypes.func.isRequired,
+    toggleOpened: PropTypes.func.isRequired
+  };
+
+  getInitState () {
+    return {
+      ready: false
+    };
+  }
+
+  @bind
+  toggleOpened () {
+    const {toggleOpened, index} = this.props;
+    toggleOpened(index);
+  }
+
+  @bind
+  onChange (value) {
+    const {onChange, index, type} = this.props;
+    onChange(index, {
+      type,
+      value
+    });
+  }
+
+  @bind
+  onRemove (event) {
+    const {onRemove, index} = this.props;
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    onRemove(index);
+  }
+
+  render () {
+    const {type} = this.props;
+    let result;
+
+    if (type === 'color') {
+      result = this.renderColor();
+    } else {
+      result = this.renderImage();
+    }
+
+    return result;
+  }
+
+  renderColor () {
+    const {value} = this.props;
+
+    return (
+      <div className={styles.colorHolder}>
+        <ColorPicker
+          value={value}
+          onChange={this.onChange}
+          className={styles.colorPicker}
+          gradients
+        />
+        <i
+          className={cx(
+            styles.removeButton,
+            'nc-icon-mini ui-1_trash-simple'
+          )}
+          onClick={this.onRemove}
+        />
+      </div>
+    );
+  }
+
+  renderImage () {
+    const {value} = this.props;
+    const style = {};
+
+    if (value.image) {
+      const url = utils.getBestImageUrl(value.image, 36, 36);
+      style.background = `url(${url})`;
+    }
+
+    return (
+      <div
+        onClick={this.toggleOpened}
+        className={styles.image}
+        ref={(ref) => {
+          this.ref = ref;
+          !this.state.ready && this.setState({ready: true});
+        }}
+      >
+        <div className={styles.preview} style={style} />
+        <div className={styles.info}>
+          {`${value.size}, x ${value.x} y ${value.y} ${value.repeat}`}
+        </div>
+        <i
+          className={cx(
+            styles.removeButton,
+            'nc-icon-mini ui-1_trash-simple'
+          )}
+          onClick={this.onRemove}
+        />
+        {this.renderImageOpened()}
+      </div>
+    );
+  }
+
+  renderImageOpened () {
+    const {value, opened} = this.props;
+
+    if (opened && this.state.ready) {
+      return (
+        <Balloon element={this.ref} stickOptions={{horizontalPosition: 'center'}}>
+          <EditImage
+            value={value}
+            onChange={this.onChange}
+          />
+        </Balloon>
+      );
+    }
+  }
+}
---
+++ lib/shared/screens/admin/shared/components/input-options/background/index.jsx
@@ -0,0 +1,124 @@
+import bind from 'decorators/bind';
+import Component from 'components/component';
+import React, {PropTypes} from 'react';
+
+import styles from './index.less';
+import Entry from './entry';
+
+export default class BackgroundInputOption extends Component {
+  static propTypes = {
+    value: PropTypes.array,
+    onChange: PropTypes.func.isRequired
+  };
+
+  static defaultProps = {
+    value: []
+  };
+
+  getInitState () {
+    return {
+      opened: false
+    };
+  }
+
+  @bind
+  toggleOpened (index) {
+    if (this.state.opened === index) {
+      this.setState({
+        opened: false
+      });
+    } else {
+      this.setState({
+        opened: index
+      });
+    }
+  }
+
+  @bind
+  onLayerChange (index, changedLayer) {
+    const {value = [], onChange} = this.props;
+    const newValue = value.slice(0);
+
+    newValue.splice(index, 1, changedLayer);
+    onChange(newValue);
+  }
+
+  addLayer (layer) {
+    const {onChange, value = []} = this.props;
+    const newValue = value.slice(0);
+
+    newValue.push(layer);
+    onChange(newValue);
+  }
+
+  @bind
+  addColorLayer () {
+    this.addLayer({
+      type: 'color',
+      value: {}
+    });
+  }
+
+  @bind
+  addImageLayer () {
+    this.addLayer({
+      type: 'image',
+      value: {
+        size: 'cover',
+        x: '50%',
+        y: '50%',
+        repeat: 'no-repeat',
+        width: '50px',
+        height: '50px'
+      }
+    });
+  }
+
+  @bind
+  onRemove (index) {
+    const {onChange, value = []} = this.props;
+    const newValue = value.slice(0);
+
+    newValue.splice(index, 1);
+    onChange(newValue);
+  }
+
+  render () {
+    const {value = []} = this.props;
+
+    return (
+      <div>
+        <div>
+          {value.map(this.renderProperty, this)}
+        </div>
+        <div className={styles.addButtons}>
+          <div className={styles.addButton} onClick={this.addColorLayer}>
+            <i className='nc-icon-mini ui-1_drop' />
+            <span>Add Color</span>
+          </div>
+          <div className={styles.addButton} onClick={this.addImageLayer}>
+            <i className='nc-icon-mini design_image' />
+            <span>Add Image</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderProperty (prop, index) {
+    const {opened} = this.state;
+
+    return (
+      <Entry
+        key={index}
+        index={index}
+        type={prop.type}
+        value={prop.value}
+        opened={opened === index}
+        onChange={this.onLayerChange}
+        onRemove={this.onRemove}
+        toggleOpened={this.toggleOpened}
+      />
+    );
+  }
+}
