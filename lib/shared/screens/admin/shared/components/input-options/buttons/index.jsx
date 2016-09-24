1b3ec59ad9bdcd9581c16d6ef3b9570fac0cea07
Bruno Mota <bruno12mota@gmail.com>
2016-09-24 18:00:05 +0100

    buttons input option

---
+++ lib/shared/screens/admin/shared/components/input-options/buttons/index.jsx
@@ -0,0 +1,52 @@
+import bind from 'decorators/bind';
+import Component from 'components/component';
+import React, {PropTypes} from 'react';
+
+import Button from './button';
+
+export default class ButtonsOptions extends Component {
+  static propTypes = {
+    values: PropTypes.array.isRequired,
+    labels: PropTypes.array.isRequired,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    vertical: PropTypes.bool,
+    white: PropTypes.bool,
+    tooltips: PropTypes.array
+  };
+
+  @bind
+  onClick (index) {
+    const {values, onChange} = this.props;
+    values[index] && onChange(values[index]);
+  }
+
+  render () {
+    const {labels} = this.props;
+
+    return (
+      <div>
+        {labels.map(this.renderButton, this)}
+      </div>
+    );
+  }
+
+  renderButton (label, index) {
+    const {vertical, labels, white, value, values, tooltips} = this.props;
+
+    return (
+      <Button
+        key={index}
+        index={index}
+        onClick={this.onClick}
+        vertical={vertical}
+        white={white}
+        selected={value === values[index]}
+        tooltip={tooltips && tooltips[index]}
+        total={labels.length}
+      >
+        {label}
+      </Button>
+    );
+  }
+}
