7ced18fa071343412c8a40ee601aa68b12f15104
Bruno Mota <bruno12mota@gmail.com>
2016-09-24 17:59:39 +0100

    background layers input option

---
+++ lib/shared/screens/admin/shared/components/input-options/background/edit-image.jsx
@@ -0,0 +1,144 @@
+import bind from 'decorators/bind';
+import Component from 'components/component';
+import OptionsList from 'components/options-list';
+import React, {PropTypes} from 'react';
+
+import styles from './edit-image.less';
+
+const options = [
+  {
+    type: 'Columns',
+    options: [
+      {
+        type: 'Image',
+        id: 'image'
+      },
+      {
+        type: 'Buttons',
+        label: 'Size',
+        id: 'size',
+        props: {
+          vertical: true,
+          values: ['cover', 'contain', 'custom'],
+          labels: ['Cover', 'Contain', 'Custom']
+        }
+      }
+    ]
+  }
+];
+
+const positionRepeatOptions = [
+  {
+    type: 'Columns',
+    options: [
+      {
+        label: 'X Position',
+        type: 'Number',
+        id: 'x',
+        props: {
+          allowed: ['%', 'px']
+        }
+      },
+      {
+        label: 'Y Position',
+        type: 'Number',
+        id: 'y',
+        props: {
+          allowed: ['%', 'px']
+        }
+      }
+    ]
+  },
+  {
+    type: 'Buttons',
+    label: 'Repeat',
+    id: 'repeat',
+    props: {
+      tooltips: ['No Repeat', 'Repeat', 'Repeat Horiz.', 'Repeat Vert.'],
+      values: ['no-repeat', 'repeat', 'repeat-x', 'repeat-y'],
+      labels: [
+        <i className='nc-icon-outline ui-1_simple-remove' />,
+        <i className='nc-icon-outline arrows-2_corner-right-down' />,
+        <i className='nc-icon-outline arrows-1_tail-right' />,
+        <i className='nc-icon-outline arrows-1_tail-down' />
+      ]
+    }
+  }
+];
+
+const customSizeOptions = [
+  {
+    type: 'Columns',
+    options: [
+      {
+        label: 'Width',
+        type: 'Number',
+        id: 'width',
+        props: {
+          allowed: ['%', 'px']
+        }
+      },
+      {
+        label: 'Height',
+        type: 'Number',
+        id: 'height',
+        props: {
+          allowed: ['%', 'px']
+        }
+      }
+    ]
+  }
+];
+
+export default class EditImage extends Component {
+  static propTypes = {
+    value: PropTypes.object.isRequired,
+    onChange: PropTypes.func.isRequired
+  };
+
+  @bind
+  onChange (key, val) {
+    const {onChange, value} = this.props;
+
+    onChange(Object.assign({}, value, {
+      [key]: val
+    }));
+  }
+
+  render () {
+    const {value} = this.props;
+
+    return (
+      <div>
+        <div className={styles.group}>
+          <OptionsList
+            options={options}
+            values={value}
+            onChange={this.onChange}
+            white
+            tight
+          />
+        </div>
+        {
+          value.size === 'custom' &&
+          <div className={styles.group}>
+            <OptionsList
+              options={customSizeOptions}
+              values={value}
+              onChange={this.onChange}
+              white
+              tight
+            />
+          </div>
+        }
+        <OptionsList
+          options={positionRepeatOptions}
+          values={value}
+          onChange={this.onChange}
+          white
+          tight
+        />
+      </div>
+    );
+  }
+}
