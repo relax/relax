4b3804478cd4d6301f17f4ad9b4f6a70520c0cae
Bruno Mota <bruno12mota@gmail.com>
2016-09-26 20:31:27 +0100

    column element background styles

--- lib/shared/elements/column/index.jsx
+++ lib/shared/elements/column/index.jsx
@@ -2,11 +2,13 @@ import React, {PropTypes} from 'react';
 
 import propsSchema from './props-schema';
 import settings from './settings';
+import style from './style';
 import Component from '../component';
 import Element from '../element';
 
 export default class Column extends Component {
   static propTypes = {
+    styleClassMap: PropTypes.object,
     padding: PropTypes.string.isRequired,
     vertical: PropTypes.string.isRequired,
     left: PropTypes.number,
@@ -23,22 +25,24 @@ export default class Column extends Component {
 
   static propsSchema = propsSchema;
   static settings = settings;
+  static style = style;
 
   render () {
+    const {styleClassMap} = this.props;
     const layout = this.props.layout || {
       width: 'auto'
     };
 
-    const style = {
+    const rootStyle = {
       display: layout.width === 'block' ? 'block' : 'table-cell',
       verticalAlign: this.props.vertical
     };
 
     if (this.props.left || this.props.right) {
-      style.padding = `0px ${this.props.right}px 0px ${this.props.left}px`;
+      rootStyle.margin = `0px ${this.props.right}px 0px ${this.props.left}px`;
     }
     if (this.props.bottom) {
-      style.marginBottom = this.props.bottom;
+      rootStyle.marginBottom = this.props.bottom;
     }
 
     const contentStyle = {
@@ -46,11 +50,11 @@ export default class Column extends Component {
     };
 
     if (layout.width !== 'block') {
-      style.width = `${layout.widthPerc}`;
+      rootStyle.width = `${layout.widthPerc}`;
     }
 
     return (
-      <div className='column' style={style}>
+      <div className={styleClassMap.root} style={rootStyle}>
         <Element {...this.props.relax} htmlTag='div' style={contentStyle} settings={settings}>
           {this.renderContent()}
         </Element>
