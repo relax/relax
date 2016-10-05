cab95cdf265b3ca5c465a5a6088d1c6a1ca4b8e3
Bruno Mota <bruno12mota@gmail.com>
2016-10-05 15:25:08 +0200

    feedback to user when editing a schema entry with a template with no data links fixes #301

---
+++ lib/shared/screens/admin/shared/components/page-builder/canvas/no-links.jsx
@@ -0,0 +1,28 @@
+import Component from 'components/component';
+import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
+
+import styles from './no-links.less';
+
+export default class NoLinks extends Component {
+  static propTypes = {
+    templateId: PropTypes.string.isRequired
+  };
+
+  render () {
+    const {templateId} = this.props;
+
+    return (
+      <div className={styles.root}>
+        <div className={styles.content}>
+          <div className={styles.title}>Hold your horses!</div>
+          <div className={styles.text}>Template you're using has no links for this content type yet!</div>
+          <Link to={`/admin/templates/${templateId}`} query={{build: 1}} className={styles.link}>
+            <i className='nc-icon-outline ui-2_share-bold' />
+            <span>Go link it</span>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
