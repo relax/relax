5487c9370196c375281a0b884b71fb45b6eb1d38
Bruno Mota <bruno12mota@gmail.com>
2016-10-06 16:02:23 +0200

    display option visible or non visible only

--- lib/shared/screens/admin/shared/components/input-options/display/index.jsx
+++ lib/shared/screens/admin/shared/components/input-options/display/index.jsx
@@ -1,32 +1,43 @@
 import bind from 'decorators/bind';
 import Component from 'components/component';
-import CssDisplay from 'components/css-display';
 import React, {PropTypes} from 'react';
 
-export default class DisplayInputOption extends Component {
+import DisplayButton from './button';
+
+export default class CssDisplayOption extends Component {
   static propTypes = {
-    value: PropTypes.object,
+    value: PropTypes.string,
     onChange: PropTypes.func.isRequired
   };
 
   @bind
-  onChange (key, val) {
-    const {onChange, value} = this.props;
-    onChange(Object.assign({}, value, {
-      [key]: val
-    }));
+  onChange (value) {
+    const {onChange} = this.props;
+    onChange(value);
   }
 
   render () {
-    const {value = {
-      display: 'block'
-    }} = this.props;
+    const {value} = this.props;
 
     return (
-      <CssDisplay
-        values={value}
-        onChange={this.onChange}
-      />
+      <div>
+        <DisplayButton
+          value=''
+          onChange={this.onChange}
+          label='Initial'
+          active={!value}
+        >
+          <i className='nc-icon-outline ui-1_eye-19' />
+        </DisplayButton>
+        <DisplayButton
+          value='none'
+          onChange={this.onChange}
+          label='Hidden'
+          active={value === 'none'}
+        >
+          <i className='nc-icon-outline ui-1_eye-ban-20' />
+        </DisplayButton>
+      </div>
     );
   }
 }
