fffc708ebaf64828d12e32c4785e5ecd22bc8644
Bruno Mota <bruno12mota@gmail.com>
2016-10-13 15:35:47 +0200

    new schema public readable and/or writable option ui

--- lib/shared/screens/admin/screens/schemas/screens/new/components/builder/name.jsx
+++ lib/shared/screens/admin/screens/schemas/screens/new/components/builder/name.jsx
@@ -1,9 +1,11 @@
-import bind from 'decorators/bind';
-import cx from 'classnames';
-import Component from 'components/component';
-import Input from 'components/modal-input';
 import React, {PropTypes} from 'react';
 
+import Component from 'components/component';
+import Input from 'components/modal-input';
+import Permission from './permission';
+import Scrollable from 'components/scrollable';
+import bind from 'decorators/bind';
+import cx from 'classnames';
 import styles from './name.less';
 
 export default class SchemaName extends Component {
@@ -11,7 +13,8 @@ export default class SchemaName extends Component {
     schema: PropTypes.object.isRequired,
     schemaStepBack: PropTypes.func.isRequired,
     schemaStepForward: PropTypes.func.isRequired,
-    changeSchemaTitle: PropTypes.func.isRequired
+    changeSchemaTitle: PropTypes.func.isRequired,
+    changeSchemaPermission: PropTypes.func.isRequired
   };
 
   @bind
@@ -21,20 +24,46 @@ export default class SchemaName extends Component {
   }
 
   render () {
-    const {schema, schemaStepBack, schemaStepForward, changeSchemaTitle} = this.props;
+    const {schema, schemaStepBack, schemaStepForward, changeSchemaTitle, changeSchemaPermission} = this.props;
     return (
-      <div className={styles.root}>
-        <div>
-          {this.renderSchemaType(schema.type)}
-        </div>
-        <form className={styles.holder} onSubmit={this.onSubmit}>
-          <div className={styles.header}>Let's give it a name!</div>
-          <Input
-            value={schema.title}
-            onChange={changeSchemaTitle}
-            focus
-            placeholder='Content type name'
-          />
+      <Scrollable>
+        <div className={styles.content}>
+          <div>
+            {this.renderSchemaType(schema.type)}
+          </div>
+          <form className={styles.holder} onSubmit={this.onSubmit}>
+            <div className={styles.header}>Let's give it a name!</div>
+            <Input
+              value={schema.title}
+              onChange={changeSchemaTitle}
+              focus
+              placeholder='Content type name'
+            />
+          </form>
+          <div className={styles.permissions}>
+            <Permission
+              value={schema.publicReadable}
+              index='publicReadable'
+              iconNegative='ui-1_eye-ban-20'
+              titleNegative='Non public readable'
+              labelNegative='This means only admins can see this schema entries'
+              iconPositive='ui-1_eye-19'
+              titlePositive='Public readable'
+              labelPositive='This means anyone can see this schema entries'
+              onChange={changeSchemaPermission}
+            />
+            <Permission
+              value={schema.publicWritable}
+              index='publicWritable'
+              iconNegative='ui-1_edit-77'
+              titleNegative='Non public writable'
+              labelNegative='This means only admins can create new entries to this schema'
+              iconPositive='ui-1_edit-76'
+              titlePositive='Public writable'
+              labelPositive='This means anyone can create new entries to this schema'
+              onChange={changeSchemaPermission}
+            />
+          </div>
           <div className={styles.buttons}>
             <div className={styles.button} onClick={schemaStepBack}>
               Back
@@ -43,8 +72,8 @@ export default class SchemaName extends Component {
               Next
             </div>
           </div>
-        </form>
-      </div>
+        </div>
+      </Scrollable>
     );
   }
 
---
+++ lib/shared/screens/admin/screens/schemas/screens/new/components/builder/permission.jsx
@@ -0,0 +1,67 @@
+import React, {PropTypes} from 'react';
+
+import Component from 'components/component';
+import bind from 'decorators/bind';
+import cx from 'classnames';
+import styles from './permission.less';
+
+export default class Permission extends Component {
+  static propTypes = {
+    value: PropTypes.bool.isRequired,
+    index: PropTypes.string.isRequired,
+    iconNegative: PropTypes.string.isRequired,
+    titleNegative: PropTypes.string.isRequired,
+    labelNegative: PropTypes.string.isRequired,
+    iconPositive: PropTypes.string.isRequired,
+    titlePositive: PropTypes.string.isRequired,
+    labelPositive: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired
+  };
+
+  @bind
+  onNegative () {
+    const {onChange, index} = this.props;
+    onChange(index, false);
+  }
+
+  @bind
+  onPositive () {
+    const {onChange, index} = this.props;
+    onChange(index, true);
+  }
+
+  render () {
+    const {
+      value,
+      iconNegative,
+      titleNegative,
+      labelNegative,
+      iconPositive,
+      titlePositive,
+      labelPositive
+    } = this.props;
+
+    return (
+      <div className={styles.root}>
+        <div className={cx(styles.option, !value && styles.active)} onClick={this.onNegative}>
+          <div className={styles.column}>
+            <i className={cx(styles.icon, 'nc-icon-outline', iconNegative)} />
+          </div>
+          <div className={styles.column}>
+            <div className={styles.title}>{titleNegative}</div>
+            <div className={styles.label}>{labelNegative}</div>
+          </div>
+        </div>
+        <div className={cx(styles.option, value && styles.active)} onClick={this.onPositive}>
+          <div className={styles.column}>
+            <i className={cx(styles.icon, 'nc-icon-outline', iconPositive)} />
+          </div>
+          <div className={styles.column}>
+            <div className={styles.title}>{titlePositive}</div>
+            <div className={styles.label}>{labelPositive}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
