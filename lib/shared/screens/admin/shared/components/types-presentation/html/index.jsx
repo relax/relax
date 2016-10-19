89194de7827f37a05f25ae72fd8fc608f8a8cffe
Bruno Mota <bruno12mota@gmail.com>
2016-10-19 21:44:48 +0200

    data content type list html presentation type

--- lib/shared/screens/admin/shared/components/types-presentation/html/index.jsx
+++ lib/shared/screens/admin/shared/components/types-presentation/html/index.jsx
@@ -1,15 +1,22 @@
-import Component from 'components/component';
 import React, {PropTypes} from 'react';
+
+import Component from 'components/component';
+import truncateHtml from 'helpers/truncate-html';
 
 export default class HtmlPresentation extends Component {
   static propTypes = {
     value: PropTypes.string
   };
 
+  shouldComponentUpdate (nextProps) {
+    return this.props.value !== nextProps.value;
+  }
+
   render () {
     const {value} = this.props;
+
     return (
-      <div dangerouslySetInnerHTML={{__html: value}} />
+      <div dangerouslySetInnerHTML={{__html: truncateHtml(value, 150)}} />
     );
   }
 }
