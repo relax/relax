d70347398387e6373e19108ebe69d79a2d4b4aa0
Bruno Mota <bruno12mota@gmail.com>
2016-10-19 21:14:52 +0200

    data content type list date presentation type

---
+++ lib/shared/screens/admin/shared/components/types-presentation/date/index.jsx
@@ -0,0 +1,26 @@
+import React, {PropTypes} from 'react';
+
+import Component from 'components/component';
+import moment from 'moment';
+
+export default class DatePresentation extends Component {
+  static propTypes = {
+    value: PropTypes.string
+  };
+
+  render () {
+    const {value} = this.props;
+    let date = 'No date selected';
+
+    if (value) {
+      const momentDate = moment(value);
+      date = momentDate.format('MMMM Do YYYY');
+    }
+
+    return (
+      <div>
+        {date}
+      </div>
+    );
+  }
+}
