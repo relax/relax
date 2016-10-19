d6601a88929427aba4bb763b0b9f1e39fdd97a65
Bruno Mota <bruno12mota@gmail.com>
2016-10-19 21:09:37 +0200

    data content type list user presentation type

---
+++ lib/shared/screens/admin/shared/components/types-presentation/user/user.jsx
@@ -0,0 +1,75 @@
+import React, {PropTypes} from 'react';
+
+import Component from 'components/component';
+import Spinner from 'components/spinner';
+import getGravatarImage from 'helpers/get-gravatar-image';
+import styles from './user.less';
+
+export default class User extends Component {
+  static fragments = {
+    user: {
+      _id: 1,
+      name: 1,
+      username: 1,
+      email: 1
+    }
+  };
+
+  static propTypes = {
+    user: PropTypes.object,
+    userId: PropTypes.string,
+    loading: PropTypes.bool
+  };
+
+  render () {
+    const {user, loading, userId} = this.props;
+    let result;
+
+    if (!userId) {
+      result = this.renderUnselected();
+    } else if (loading) {
+      result = this.renderLoading();
+    } else if (user) {
+      result = this.renderUser();
+    } else {
+      result = this.renderError();
+    }
+
+    return result;
+  }
+
+  renderLoading () {
+    return (
+      <Spinner />
+    );
+  }
+
+  renderError () {
+    return (
+      <div>User not found</div>
+    );
+  }
+
+  renderUnselected () {
+    return (
+      <div>None selected</div>
+    );
+  }
+
+  renderUser () {
+    const {user} = this.props;
+    const url = getGravatarImage(user.email, 30);
+
+    return (
+      <div className={styles.root}>
+        <div className={styles.image}>
+          <img src={url} role='presentation' />
+        </div>
+        <div className={styles.info}>
+          <div className={styles.name}>{user.name}</div>
+          <div className={styles.username}>{user.username}</div>
+        </div>
+      </div>
+    );
+  }
+}
