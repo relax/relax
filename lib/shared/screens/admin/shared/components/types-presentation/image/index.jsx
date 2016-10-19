97d09cdc1e1d86fe2174c42eb27e47cca0616dec
Bruno Mota <bruno12mota@gmail.com>
2016-10-19 23:16:39 +0200

    data content type list image presentation type

--- lib/shared/screens/admin/shared/components/types-presentation/image/index.jsx
+++ lib/shared/screens/admin/shared/components/types-presentation/image/index.jsx
@@ -1,20 +1,51 @@
-import Component from 'components/component';
-import Image from 'components/image';
 import React, {PropTypes} from 'react';
 
+import Component from 'components/component';
+import MediaItem from 'components/media-item-preview';
+import {dataConnect} from 'relate-js';
+import styles from './index.less';
+
+@dataConnect(
+  (props) => {
+    let result = {};
+
+    if (props.value) {
+      result = {
+        fragments: MediaItem.fragments,
+        variablesTypes: {
+          mediaItem: {
+            id: 'ID!'
+          }
+        },
+        initialVariables: {
+          mediaItem: {
+            id: props.value
+          }
+        }
+      };
+    }
+
+    return result;
+  }
+)
 export default class ImagePresentation extends Component {
   static propTypes = {
-    value: PropTypes.string.isRequired
+    value: PropTypes.string.isRequired,
+    mediaItem: PropTypes.object
   };
 
   render () {
-    const {value} = this.props;
+    const {mediaItem} = this.props;
+
     return (
-      <Image
-        id={value}
-        width={80}
-        height={80}
-      />
+      <div className={styles.root}>
+        <MediaItem
+          mediaItem={mediaItem}
+          width={80}
+          height={80}
+          useThumbnail
+        />
+      </div>
     );
   }
 }
