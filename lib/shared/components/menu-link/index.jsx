2ef5d67a59f2f02e6f23bf4a8d47990b09071c2e
Bruno Mota <bruno12mota@gmail.com>
2016-10-20 20:32:53 +0200

    menu element rearrangement

---
+++ lib/shared/components/menu-link/index.jsx
@@ -0,0 +1,63 @@
+import React, {PropTypes} from 'react';
+
+import A from 'components/a';
+import Component from 'components/component';
+
+export default class MenuLink extends Component {
+  static propTypes = {
+    type: PropTypes.string.isRequired,
+    editing: PropTypes.bool,
+    label: PropTypes.string,
+    className: PropTypes.string,
+    url: PropTypes.string
+  };
+
+  static defaultProps = {
+    url: '#'
+  };
+
+  render () {
+    const {editing, type} = this.props;
+    let result;
+
+    if (editing) {
+      result = this.renderPlaceholder();
+    } else if (type === 'url') {
+      result = this.renderExternal();
+    } else {
+      result = this.renderInternal();
+    }
+
+    return result;
+  }
+
+  renderPlaceholder () {
+    const {label, className} = this.props;
+
+    return (
+      <a className={className}>
+        {label}
+      </a>
+    );
+  }
+
+  renderExternal () {
+    const {label, className, url} = this.props;
+
+    return (
+      <a className={className} href={url}>
+        {label}
+      </a>
+    );
+  }
+
+  renderInternal () {
+    const {label, className, url} = this.props;
+
+    return (
+      <A className={className} href={url}>
+        {label}
+      </A>
+    );
+  }
+}
---
+++ lib/shared/elements/menu/button.jsx
@@ -0,0 +1,97 @@
+import React, {PropTypes} from 'react';
+
+import Animate from 'components/animate';
+import Component from 'components/component';
+import MenuLink from 'components/menu-link';
+import SubButton from './sub-button';
+import bind from 'decorators/bind';
+import classes from './classes';
+import cx from 'classnames';
+
+export default class MenuElementButton extends Component {
+  static propTypes = {
+    button: PropTypes.object.isRequired,
+    styleClassMap: PropTypes.object.isRequired,
+    menuData: PropTypes.object.isRequired,
+    editing: PropTypes.bool
+  };
+
+  getInitState () {
+    return {
+      opened: false
+    };
+  }
+
+  componentWillUnmount () {
+    clearTimeout(this.closeTimeout);
+  }
+
+  @bind
+  onMouseOver () {
+    clearTimeout(this.closeTimeout);
+    this.setState({
+      opened: true
+    });
+  }
+
+  @bind
+  onMouseOut () {
+    this.closeTimeout = setTimeout(this.close, 400);
+  }
+
+  @bind
+  close () {
+    this.setState({
+      opened: false
+    });
+  }
+
+  render () {
+    const {styleClassMap, button, editing} = this.props;
+
+    return (
+      <li
+        className={cx(classes.menuItem, styleClassMap.entry)}
+        onMouseEnter={this.onMouseOver}
+        onMouseLeave={this.onMouseOut}
+      >
+        <MenuLink
+          {...button}
+          editing={editing}
+          className={cx(classes.button, styleClassMap.button)}
+        />
+        {this.renderSubMenu()}
+      </li>
+    );
+  }
+
+  renderSubMenu () {
+    const {button, styleClassMap} = this.props;
+    const {opened} = this.state;
+
+    if (button.children && opened) {
+      return (
+        <Animate transition='fadeIn'>
+          <ul className={cx(classes.submenu, styleClassMap.submenu)}>
+            {button.children.map(this.renderSubButton, this)}
+          </ul>
+        </Animate>
+      );
+    }
+  }
+
+  renderSubButton (id) {
+    const {styleClassMap, menuData, editing} = this.props;
+    const button = menuData[id];
+
+    return (
+      <SubButton
+        styleClassMap={styleClassMap}
+        button={button}
+        editing={editing}
+        menuData={menuData}
+        key={id}
+      />
+    );
+  }
+}
