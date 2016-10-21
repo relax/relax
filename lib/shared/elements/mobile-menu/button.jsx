703bb2ed51ff3dab37acef20266255ecc21aef23
Bruno Mota <bruno12mota@gmail.com>
2016-10-21 19:35:52 +0200

    mobile menu element fixes #35

---
+++ lib/shared/elements/mobile-menu/button.jsx
@@ -0,0 +1,58 @@
+import React, {PropTypes} from 'react';
+
+import Component from 'components/component';
+import MenuLink from 'components/menu-link';
+import SubButton from './sub-button';
+import classes from './classes';
+import cx from 'classnames';
+
+export default class MenuElementButton extends Component {
+  static propTypes = {
+    button: PropTypes.object.isRequired,
+    styleClassMap: PropTypes.object.isRequired,
+    menuData: PropTypes.object.isRequired,
+    editing: PropTypes.bool
+  };
+
+  render () {
+    const {styleClassMap, button, editing} = this.props;
+
+    return (
+      <li className={styleClassMap.menuButtonLi}>
+        <MenuLink
+          {...button}
+          editing={editing}
+          className={cx(classes.menuButton, styleClassMap.menuButton)}
+        />
+        {this.renderSubMenu()}
+      </li>
+    );
+  }
+
+  renderSubMenu () {
+    const {button, styleClassMap} = this.props;
+
+    if (button.children) {
+      return (
+        <ul className={cx(classes.submenu, styleClassMap.submenu)}>
+          {button.children.map(this.renderSubButton, this)}
+        </ul>
+      );
+    }
+  }
+
+  renderSubButton (id) {
+    const {styleClassMap, menuData, editing} = this.props;
+    const button = menuData[id];
+
+    return (
+      <SubButton
+        styleClassMap={styleClassMap}
+        button={button}
+        editing={editing}
+        menuData={menuData}
+        key={id}
+      />
+    );
+  }
+}
