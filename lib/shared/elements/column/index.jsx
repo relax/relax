ff4a3c27435e74cf3d88c490523b77c016403bdd
Bruno Mota <bruno12mota@gmail.com>
2016-10-27 20:44:27 +0200

    column element refactor

--- lib/shared/elements/column/index.jsx
+++ lib/shared/elements/column/index.jsx
@@ -1,61 +1,55 @@
+import css from 'helpers/styles/css';
 import React, {PropTypes} from 'react';
 
+import Component from '../component';
+import Element from '../element';
 import propsSchema from './props-schema';
 import settings from './settings';
 import style from './style';
-import Component from '../component';
-import Element from '../element';
 
 export default class Column extends Component {
   static propTypes = {
-    styleClassMap: PropTypes.object,
-    padding: PropTypes.string.isRequired,
-    vertical: PropTypes.string.isRequired,
+    relax: PropTypes.object.isRequired,
+    styleClassMap: PropTypes.object.isRequired,
+    type: PropTypes.string,
     left: PropTypes.number,
-    right: PropTypes.number,
-    bottom: PropTypes.number,
-    layout: PropTypes.object,
-    relax: PropTypes.object.isRequired
-  };
-
-  static defaultProps = {
-    padding: '15px',
-    vertical: 'top'
+    right: PropTypes.number
   };
 
   static propsSchema = propsSchema;
   static settings = settings;
   static style = style;
 
+  getStyles () {
+    const {type} = this.props;
+    const styles = {
+      root: {}
+    };
+
+    if (type !== 'block') {
+      const {left, right} = this.props;
+
+      css(styles.root)
+        .setProperty('display', 'table-cell')
+        .setProperty('paddingLeft', left)
+        .setProperty('paddingRight', right);
+    }
+
+    return styles;
+  }
+
   render () {
-    const {styleClassMap} = this.props;
-    const layout = this.props.layout || {
-      width: 'auto'
-    };
-
-    const rootStyle = {
-      display: layout.width === 'block' ? 'block' : 'table-cell',
-      verticalAlign: this.props.vertical
-    };
-
-    if (this.props.left || this.props.right) {
-      rootStyle.margin = `0px ${this.props.right}px 0px ${this.props.left}px`;
-    }
-    if (this.props.bottom) {
-      rootStyle.marginBottom = this.props.bottom;
-    }
-
-    const contentStyle = {
-      padding: this.props.padding
-    };
-
-    if (layout.width !== 'block') {
-      rootStyle.width = `${layout.widthPerc}`;
-    }
+    const {styleClassMap, relax} = this.props;
+    const styles = this.getStyles();
 
     return (
-      <div className={styleClassMap.root} style={rootStyle}>
-        <Element {...this.props.relax} htmlTag='div' style={contentStyle} settings={settings}>
+      <div className={styleClassMap.root} style={styles.root}>
+        <Element
+          {...relax}
+          htmlTag='div'
+          className={styleClassMap.content}
+          settings={settings}
+        >
           {this.renderContent()}
         </Element>
       </div>
