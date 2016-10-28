7ae2ad90c0e90c4d10c088bb51fdb1878df41c55
Bruno Mota <bruno12mota@gmail.com>
2016-10-28 16:46:06 +0200

    style tab disabled for template elements

--- lib/shared/screens/admin/shared/components/page-builder-menu/tabs/style/style-tab.jsx
+++ lib/shared/screens/admin/shared/components/page-builder-menu/tabs/style/style-tab.jsx
@@ -7,15 +7,18 @@ import TabEmpty from '../tab-empty';
 export default class StyleTab extends Component {
   static propTypes = {
     stylable: PropTypes.bool.isRequired,
-    eligable: PropTypes.bool.isRequired
+    eligable: PropTypes.bool.isRequired,
+    isTemplate: PropTypes.bool.isRequired
   };
 
   render () {
-    const {stylable, eligable} = this.props;
+    const {stylable, eligable, isTemplate} = this.props;
     let result;
 
     if (!eligable) {
       result = this.renderNoneSelected();
+    } else if (isTemplate) {
+      result = this.renderIsTemplate();
     } else if (stylable) {
       result = this.renderStylePicker();
     } else {
@@ -28,6 +31,14 @@ export default class StyleTab extends Component {
   renderStylePicker () {
     return (
       <StylePicker />
+    );
+  }
+
+  renderIsTemplate () {
+    return (
+      <TabEmpty icon='nc-icon-outline ui-1_circle-remove'>
+        Current selected element is from the template and can only be edit there.
+      </TabEmpty>
     );
   }
 
