4bffdb676fd30a5bc4d4eef519f54fb35725ac7b
Bruno Mota <bruno12mota@gmail.com>
2016-11-01 19:09:48 +0100

    import and export settings coming soon label

--- lib/shared/screens/admin/screens/settings/screens/data/components/data.jsx
+++ lib/shared/screens/admin/screens/settings/screens/data/components/data.jsx
@@ -1,12 +1,12 @@
 import Component from 'components/component';
 import React, {PropTypes} from 'react';
 
-export default class DataSettings extends Component {
+export default class ImportExportSettings extends Component {
   static propTypes = {};
 
   render () {
     return (
-      <div>Data Settings</div>
+      <div>Coming soon</div>
     );
   }
 }
