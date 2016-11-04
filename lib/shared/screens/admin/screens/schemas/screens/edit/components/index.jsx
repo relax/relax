2361febba3d64a1e5f757ea6b999f57d74b5999c
Bruno Mota <bruno12mota@gmail.com>
2016-11-04 11:59:11 +0100

    edit schema route wireframe

---
+++ lib/shared/screens/admin/screens/schemas/screens/edit/components/index.jsx
@@ -0,0 +1,103 @@
+import Component from 'components/component';
+import Content from 'components/content';
+import ContentHeader from 'components/content-header';
+import ContentHeaderActions from 'components/content-header-actions';
+import ContentLoading from 'components/content-loading';
+import ContentNotFound from 'components/content-not-found';
+import ModalDelete from 'components/modal-delete';
+import Properties from 'components/properties';
+import React, {PropTypes} from 'react';
+
+import styles from './index.less';
+
+export default class SchemaEdit extends Component {
+  static fragments = {
+    schema: {
+      _id: 1,
+      type: 1,
+      title: 1,
+      publicWritable: 1,
+      publicReadable: 1,
+      properties: 1
+    }
+  };
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    schema: PropTypes.object,
+    removeConfirm: PropTypes.bool,
+    toggleRemoveConfirm: PropTypes.func.isRequired,
+    confirmRemove: PropTypes.func.isRequired
+  }
+
+  render () {
+    const {schema, loading} = this.props;
+    let result;
+
+    if (loading) {
+      result = this.renderLoading();
+    } else if (!schema) {
+      result = this.renderNotFound();
+    } else {
+      result = this.renderContent();
+    }
+
+    return result;
+  }
+
+  renderLoading () {
+    return (
+      <ContentLoading />
+    );
+  }
+
+  renderNotFound () {
+    return (
+      <ContentNotFound name='schema' />
+    );
+  }
+
+  renderContent () {
+    const {schema, toggleRemoveConfirm} = this.props;
+
+    return (
+      <div>
+        <ContentHeader smallPadding>
+          <div className={styles.headerLabel}>
+            {`Editing ${schema.title} content type`}
+          </div>
+          <ContentHeaderActions>
+            <button
+              className={styles.actionButton}
+              onClick={toggleRemoveConfirm}
+            >
+              <i className='nc-icon-outline ui-1_trash'></i>
+            </button>
+          </ContentHeaderActions>
+        </ContentHeader>
+        <Content>
+          <div className={styles.form}>
+            <Properties />
+          </div>
+        </Content>
+        {this.renderRemoveConfirm()}
+      </div>
+    );
+  }
+
+  renderRemoveConfirm () {
+    const {removeConfirm, toggleRemoveConfirm, confirmRemove} = this.props;
+
+    if (removeConfirm) {
+      const {schema} = this.props;
+
+      return (
+        <ModalDelete
+          title={`Are you sure you want to remove "${schema.title}"?`}
+          cancel={toggleRemoveConfirm}
+          submit={confirmRemove}
+        />
+      );
+    }
+  }
+}
