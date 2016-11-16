dff85f4fa1205c9035e0b7116d02bb12ef1dac40
Layton Whiteley <layton.whiteley@gmail.com>
2016-11-16 10:47:23 -0400

    chore(start): format start msg in console

--- app.js
+++ app.js
@@ -6,13 +6,23 @@ import app from './lib/server';
 import config from './config';
 import logger from './lib/server/logger';
 import migrate from './lib/server/migrate';
+import {padStart} from 'lodash';
+
+const outline = padStart('', 20, '-');
+logger.debug(`\n${outline}\nRelax Status\n${outline}`);
 
 // Connect mongoose
 if (!config.db) {
-  throw new Error('Configuration to MongoDB required');
+  throw new Error('DB: Configuration for MongoDB required');
 }
 mongoose.Promise = global.Promise; // Use native promises
-mongoose.connect(config.db.uri, config.db);
+mongoose.connect(config.db.uri, config.db, (err) => {
+  if (err) {
+    logger.debug('DB: Not Connected');
+  }else{
+    logger.debug('DB: Connected');
+  }
+});
 
 
 // Run migrations
@@ -21,7 +31,8 @@ migrate()
     // Start server
     const server = app.listen(config.port, () => {
       const port = server.address().port;
-      logger.debug('Listening at port', port);
+      logger.debug('URL: http://localhost:%s', port);
+      logger.debug('PORT: %s', port);
     });
   })
   .done();
